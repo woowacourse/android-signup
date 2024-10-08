38795e32f3686102fd5bd54260c147e07a04e690
ii2001 <iicks2001@gmail.com>
2024-10-08 12:31:53 +0900

    :lipstick: step2구현

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,13 +3,25 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +29,100 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface {
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        SignUpLabel()
+                        SignUpScreen()
+                    }
                 }
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignUpLabel() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Welcome to Compose🚀",
+            modifier = Modifier
+                .align(Alignment.Center),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun SignUpScreen() {
+    var userName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordConfirm by remember { mutableStateOf("") }
+
+    Column {
+        TextField(
+            value = userName,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            onValueChange = { userName = it },
+            label = { Text("Username") }
+        )
+
+        TextField(
+            value = email,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+        TextField(
+            value = password,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            onValueChange = { password = it },
+            label = { Text("Password") }
+        )
+        TextField(
+            value = passwordConfirm,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            onValueChange = { passwordConfirm = it },
+            label = { Text("Password Confirm") }
+        )
+        Button(
+            onClick = { /* Sign up logic */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp)
+        ) {
+            Text(text = "Sign Up")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignUpPreview() {
     SignupTheme {
-        Greeting("Android")
+        Surface {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                SignUpLabel()
+                SignUpScreen()
+            }
+        }
     }
 }
