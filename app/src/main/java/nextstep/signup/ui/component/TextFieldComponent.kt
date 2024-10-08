4631c4d5fcdf6031d35d2a52b3007d9a1987bae1
김진우 <jinwoo3661@naver.com>
2024-10-08 21:17:25 +0900

    feat: 커스텀 텍스트 필드 생성

---
+++ app/src/main/java/nextstep/signup/ui/component/TextFieldComponent.kt
@@ -0,0 +1,67 @@
+package nextstep.signup.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.signup.ui.theme.Blue50
+import nextstep.signup.ui.theme.BlueGray20
+import nextstep.signup.ui.theme.Gray50
+
+
+@Composable
+fun TextFieldComponent(
+    textValue: String,
+    onTextChange: (newText: String) -> Unit,
+    labelText: String,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 18.dp)
+    ) {
+        TextField(
+            value = textValue,
+            onValueChange = onTextChange,
+            label = { Text(text = labelText) },
+            modifier = Modifier
+                .background(color = BlueGray20)
+                .fillMaxWidth(),
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedLabelColor = Blue50,
+                unfocusedLabelColor = Gray50,
+                unfocusedContainerColor = BlueGray20,
+                focusedContainerColor = BlueGray20,
+                focusedIndicatorColor = Blue50,
+                unfocusedIndicatorColor = Color.Black,
+            ),
+            textStyle = TextStyle(
+                color = Gray50,
+                fontSize = 16.sp,
+            )
+        )
+    }
+}
+
+@Preview(
+    backgroundColor = 0xFFFFFFFF,
+    showBackground = true
+)
+@Composable
+fun PreviewTextFieldComponent() {
+    TextFieldComponent(
+        textValue = "",
+        onTextChange = {},
+        labelText = "",
+    )
+}
