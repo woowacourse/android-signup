b1c13df8d02d5a87ee5e8738f1a0371a2b731df4
김진우 <jinwoo3661@naver.com>
2024-10-08 21:24:20 +0900

    refactor : 네이밍 수정

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,7 +3,6 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SignupView()
+                    SignupScreen()
                 }
             }
         }
@@ -47,7 +45,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SignupView(){
+fun SignupScreen(){
     var userName by remember { mutableStateOf(DEFAULT_TEXT) }
     var email by remember { mutableStateOf(DEFAULT_TEXT) }
     var password by remember { mutableStateOf(DEFAULT_TEXT) }
@@ -107,5 +105,5 @@ fun SignupView(){
 )
 @Composable
 fun PreviewSignupView(){
-    SignupView()
+    SignupScreen()
 }
