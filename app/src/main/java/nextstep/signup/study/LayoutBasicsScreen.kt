842dab5a606d7fa36d37de8cc53e61861adad53c
sh1mj1 <7wlgns@gmail.com>
2024-10-09 15:30:03 +0900

    study(KimposeKKmPose): KimposeKKmPose, KimposeKKmPosePreview

--- app/src/main/java/nextstep/signup/study/LayoutBasicsScreen.kt
+++ app/src/main/java/nextstep/signup/study/LayoutBasicsScreen.kt
@@ -1,9 +1,11 @@
 package nextstep.signup.study
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -28,4 +30,21 @@ fun HelloCompose(text: String, modifier: Modifier = Modifier) {
 @Composable
 private fun HelloComposePreview() {
     HelloCompose("안녕 난 컴포즈야~")
+}
+
+@Composable
+fun KimposeKKmPose(modifier: Modifier = Modifier.testTag("이름")) {
+    Column(
+        modifier = modifier
+    ) {
+        // 바꿔 보세요!
+        Text(text = "킴포즈", color = Color.Cyan)
+        Text(text = "끔포즈", color = Color.Yellow)
+    }
+}
+
+@Preview
+@Composable
+private fun KimposeKKmPosePreview() {
+    KimposeKKmPose()
 }
