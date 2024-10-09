5ec8cc0ae990c7a64e31fe98ffac356146284a2c
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-09 15:43:40 +0900

    feat: 버튼 컴포넌트 추가

---
+++ app/src/main/java/nextstep/signup/ui/Buttons.kt
@@ -0,0 +1,43 @@
+package nextstep.signup.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DefaultButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    fontSize: TextUnit,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+    ) {
+        Text(
+            modifier = Modifier.padding(vertical = 16.dp),
+            text = text,
+            fontSize = fontSize,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DefaultButtonPreview() {
+    DefaultButton(
+        modifier = Modifier
+            .fillMaxWidth(),
+        text = "Sign Up",
+        fontSize = 14.sp,
+        onClick = {},
+    )
+}
