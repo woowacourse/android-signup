d92a67324b9c11671081e29303e8a8ec3e7f740b
sh1mj1 <7wlgns@gmail.com>
2024-10-09 17:13:51 +0900

    feat(SignUpScreen): SignUpTextField

--- app/src/androidTest/java/nextstep/signup/SignUpScreenKtTest.kt
+++ app/src/androidTest/java/nextstep/signup/SignUpScreenKtTest.kt
@@ -1,7 +1,13 @@
 package nextstep.signup
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,6 +26,25 @@ class SignUpScreenKtTest {
         composeTestRule
             .onNodeWithText("Welcome to Compose 🚀")
             .assertExists()
+    }
+
+
+    @Test
+    fun signUpTextField() {
+        // given
+        composeTestRule.setContent {
+            SignUpTextField(
+                modifier = Modifier.testTag("text_field")
+            )
+        }
+
+        // when
+        composeTestRule.onNodeWithTag("text_field")
+            .performTextInput("sh1mj1")
+
+        // then
+        composeTestRule.onNodeWithTag("text_field")
+            .assert(hasText("sh1mj1"))
     }
 
 
--- app/src/main/java/nextstep/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/SignUpScreen.kt
@@ -1,7 +1,12 @@
 package nextstep.signup
 
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -14,7 +19,7 @@ fun SignUpHeader(modifier: Modifier = Modifier) {
     Text(
         text = "Welcome to Compose 🚀",
         style = TextStyle(
-            fontSize =26.sp,
+            fontSize = 26.sp,
             fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.SansSerif
 
@@ -22,8 +27,28 @@ fun SignUpHeader(modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun SignUpTextField(modifier: Modifier = Modifier, labelText: String = "Enter Text") {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        maxLines = 1,
+        label = { Text(text = labelText) },
+        modifier = modifier,
+    )
+}
+
+
 @Preview
 @Composable
 private fun SignUpHeaderPreview() {
     SignUpHeader()
+}
+
+@Preview
+@Composable
+private fun SignUpTextFieldPreview() {
+    SignUpTextField()
 }
