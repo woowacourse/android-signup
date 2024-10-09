c73f64b8ec71ac3aaf29e240db6702ddf9e6eb96
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-09 15:43:28 +0900

    feat: 헤드라인에 들어갈 글자 스타일 추가

--- app/src/main/java/nextstep/signup/ui/theme/Type.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Type.kt
@@ -14,7 +14,15 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
