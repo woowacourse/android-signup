25bef03e1a7421ba5ae5c1c638d258643314d259
JunJangE <wnswkd486@gmail.com>
2024-10-09 23:56:46 +0900

    style: ktlint

--- app/src/main/java/nextstep/signup/componet/CustomText.kt
+++ app/src/main/java/nextstep/signup/componet/CustomText.kt
@@ -4,8 +4,6 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
--- app/src/main/java/nextstep/signup/ui/MainScreen.kt
+++ app/src/main/java/nextstep/signup/ui/MainScreen.kt
@@ -1,10 +1,16 @@
 package nextstep.signup.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
