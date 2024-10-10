ff69ce9ce5da4dc4ad2040df5b0ed74125465d84
somin <salth0601@gmail.com>
2024-10-10 16:23:57 +0900

    ui: 회원가입 TextField 추가

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -26,13 +29,17 @@ class MainActivity : ComponentActivity() {
             SignupTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween,
+                        verticalArrangement = Arrangement.Top,
                     ) {
                         SignupTitle(Modifier.padding(top = 60.dp))
+                        SingUpTextField(Modifier.padding(top = 42.dp), "Username")
+                        SingUpTextField(Modifier.padding(top = 36.dp), "email")
+                        SingUpTextField(Modifier.padding(top = 36.dp), "Password")
+                        SingUpTextField(Modifier.padding(top = 36.dp), "Password Confirm")
                     }
                 }
             }
@@ -47,6 +54,22 @@ fun SignupTitle(modifier: Modifier = Modifier) {
         modifier = modifier,
         fontSize = 26.sp,
         fontWeight = FontWeight.Bold,
+    )
+}
+
+@Composable
+fun SingUpTextField(
+    modifier: Modifier = Modifier,
+    hint: String,
+) {
+    val enteredValue = remember { mutableStateOf("") }
+    TextField(
+        value = enteredValue.value,
+        onValueChange = { enteredValue.value = it },
+        modifier = modifier,
+        maxLines = 1,
+        placeholder = { Text(hint) },
+        label = { Text(hint) },
     )
 }
 
