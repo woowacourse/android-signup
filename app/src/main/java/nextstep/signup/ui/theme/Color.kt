4ff6dbde4f701b53ddd22532043907d7e5035e29
murjune <sis9221302@gmail.com>
2024-10-10 15:52:57 +0900

    Color Themeing

--- app/src/main/java/nextstep/signup/ui/theme/Color.kt
+++ app/src/main/java/nextstep/signup/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Custom color
+val Gray80 = Color(0xFF49454F)
+val Blue50 = Color(0xFF2196F3)
+val BlueGrey20 = Color(0xFFE3E8F1)
+val White = Color(0xFFFFFFFF)
+val Red60 = Color(0xFFFF0000)
+val Red20 = Color(0xFFFFEBEE)
