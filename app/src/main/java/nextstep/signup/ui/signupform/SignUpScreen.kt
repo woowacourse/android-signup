615019d22f73b60466f05ae79b4c0eb04ad94236
aprilgom <aprilgom@gmail.com>
2024-10-10 13:47:02 +0900

    refactor: 문자열 상수화

--- app/src/main/java/nextstep/signup/ui/signupform/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/signupform/SignUpScreen.kt
@@ -7,10 +7,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import nextstep.signup.R
 import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
@@ -20,16 +22,19 @@ fun SignUpForm(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = "Welcome to Compose \uD83D\uDE80",
+            text = stringResource(id = R.string.welcome),
             fontSize = 26.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(bottom = 42.dp),
         )
-        SignUpTextField("UserName", modifier = Modifier.padding(bottom = 36.dp))
-        SignUpTextField("Email", modifier = Modifier.padding(bottom = 36.dp))
-        SignUpPasswordTextField("Password", modifier = Modifier.padding(bottom = 36.dp))
-        SignUpPasswordTextField("Password Confirm", modifier = Modifier.padding(bottom = 42.dp))
-        ConfirmButton("Sign Up", modifier = Modifier.fillMaxWidth())
+        SignUpTextField(
+            stringResource(id = R.string.user_name),
+            modifier = Modifier.padding(bottom = 36.dp)
+        )
+        SignUpTextField(stringResource(id = R.string.email), modifier = Modifier.padding(bottom = 36.dp))
+        SignUpPasswordTextField(stringResource(id = R.string.password), modifier = Modifier.padding(bottom = 36.dp))
+        SignUpPasswordTextField(stringResource(id = R.string.password_confirm), modifier = Modifier.padding(bottom = 42.dp))
+        ConfirmButton(stringResource(id = R.string.signup), modifier = Modifier.fillMaxWidth())
     }
 }
 
