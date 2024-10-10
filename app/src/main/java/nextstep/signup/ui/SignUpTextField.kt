f6bc5353edec93f527b67abd8a397b0836bfb7db
somin <salth0601@gmail.com>
2024-10-10 17:34:54 +0900

    ui: TextField 색상 변경 및 비밀번호 숨기기

--- app/src/main/java/nextstep/signup/ui/SignUpTextField.kt
+++ app/src/main/java/nextstep/signup/ui/SignUpTextField.kt
@@ -3,17 +3,23 @@ package nextstep.signup.ui
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.signup.R
 import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
 fun SignUpTextField(
     modifier: Modifier = Modifier,
     hint: String,
+    isPassword: Boolean,
 ) {
     val enteredValue = remember { mutableStateOf("") }
     TextField(
@@ -21,10 +27,19 @@ fun SignUpTextField(
         onValueChange = { enteredValue.value = it },
         modifier = modifier.fillMaxWidth(),
         maxLines = 1,
-        placeholder = { Text(hint) },
-        label = { Text(hint) },
+        placeholder = { Text(text = hint) },
+        label = { Text(text = hint) },
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = colorResource(id = R.color.blue_50),
+            focusedLabelColor = colorResource(id = R.color.blue_50),
+        ),
+        visualTransformation = setVisualTransformation(isPassword),
     )
 }
+
+@Composable
+private fun setVisualTransformation(isPassword: Boolean) =
+    if (isPassword) PasswordVisualTransformation() else VisualTransformation.None
 
 @Preview(showBackground = true)
 @Composable
@@ -33,6 +48,7 @@ fun SignUpTextFieldPreview() {
         SignUpTextField(
             Modifier,
             "미리 보기 텍스트 필드",
+            true,
         )
     }
 }
