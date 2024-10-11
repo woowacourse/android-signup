a092a221c5b90981cbb3c3bd0d3b20d5cf379b9a
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-11 17:22:19 +0900

    feat: 이름 입력 텍스트 필드

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.signup.ui.theme.SignupTheme
@@ -27,13 +38,25 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(top = 50.dp),
+                        .padding(
+                            start = 32.dp,
+                            top = 50.dp,
+                            end = 32.dp,
+                        ),
                     color = MaterialTheme.colorScheme.background,
                 ) {
+                    var username by rememberSaveable { mutableStateOf("") }
+
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         HeadLine(getString(R.string.sign_up_headline))
+                        Spacer(modifier = Modifier.height(36.dp))
+                        SignUpTextField(
+                            value = username,
+                            onValueChange = { username = it },
+                            label = getString(R.string.sign_up_username_label),
+                        )
                     }
 
                 }
@@ -49,5 +72,32 @@ fun HeadLine(text: String) {
         color = Color.Black,
         fontSize = 26.sp,
         fontWeight = FontWeight.Bold,
+    )
+}
+
+@Composable
+fun SignUpTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+    singleLine: Boolean = true,
+    focusedLabelColor: Color = colorResource(id = R.color.blue50),
+    focusedIndicatorColor: Color = colorResource(id = R.color.blue50),
+    cursorColor: Color = colorResource(id = R.color.blue50)
+) {
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = label) },
+        singleLine = singleLine,
+        keyboardOptions = keyboardOptions,
+        colors = TextFieldDefaults.colors(
+            focusedLabelColor = focusedLabelColor,
+            focusedIndicatorColor = focusedIndicatorColor,
+            cursorColor = cursorColor
+        )
     )
 }
