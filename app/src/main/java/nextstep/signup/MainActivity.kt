4b2a028f8c37f9473771eef2fb9abd7d364edb8c
somin <salth0601@gmail.com>
2024-10-11 21:38:58 +0900

    refactor: rememberSaveable로 Configuration Change 대응

--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import nextstep.signup.ui.SignUpScreen
 import nextstep.signup.ui.theme.SignupTheme
@@ -14,10 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var email by remember { mutableStateOf("") }
-            var userName by remember { mutableStateOf("") }
-            var password by remember { mutableStateOf("") }
-            var passwordConfirm by remember { mutableStateOf("") }
+            var email by rememberSaveable { mutableStateOf("") }
+            var userName by rememberSaveable { mutableStateOf("") }
+            var password by rememberSaveable { mutableStateOf("") }
+            var passwordConfirm by rememberSaveable { mutableStateOf("") }
 
             SignupTheme {
                 SignUpScreen(
