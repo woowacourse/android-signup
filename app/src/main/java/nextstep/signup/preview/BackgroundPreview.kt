b96152e3f3ba64818b15293222fcb58ee0855555
murjune <sis9221302@gmail.com>
2024-10-11 12:51:41 +0900

    BackgroundPreview 어노테이션

---
+++ app/src/main/java/nextstep/signup/preview/BackgroundPreview.kt
@@ -0,0 +1,10 @@
+package nextstep.signup.preview
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    backgroundColor = 0xFFFFFFFF
+)
+annotation class BackgroundPreview
