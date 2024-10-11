9654b89cb35d543d724ba74f6eff24f5f257009c
Hogu59 <dbstjdguswkd@naver.com>
2024-10-12 00:31:00 +0900

    rename: move MainActivity to presentation

---
+++ app/src/main/java/nextstep/signup/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package nextstep.signup.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import nextstep.signup.presentation.components.signup.SignUpForm
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SignUpForm()
+        }
+    }
+}
