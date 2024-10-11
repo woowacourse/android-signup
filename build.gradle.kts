df5e955dd8d33b8c838194c4956b81dfee241dce
sh1mj1 <7wlgns@gmail.com>
2024-10-11 14:36:07 +0900

    build: add junit5, kotest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+allprojects{
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
