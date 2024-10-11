350776d18966593f760134e6dfc3adb5d9943abd
sh1mj1 <7wlgns@gmail.com>
2024-10-11 14:45:23 +0900

    feat(Password)

---
+++ app/src/main/java/nextstep/signup/domain/Password.kt
@@ -0,0 +1,8 @@
+package nextstep.signup.domain
+
+data class Password(
+    val password: String,
+    val passwordConfirm: String,
+) {
+    fun isValid(): Boolean = password.isNotBlank() && password == passwordConfirm
+}
---
+++ app/src/test/java/nextstep/signup/domain/PasswordTest.kt
@@ -0,0 +1,32 @@
+package nextstep.signup.domain
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class PasswordTest {
+    @Test
+    fun `공백이면 유효하지 않은 패스워드`() {
+        // given
+        val password = Password(
+            password = "",
+            passwordConfirm = "",
+        )
+
+        // then
+        password.isValid() shouldBe false
+    }
+
+
+    @Test
+    fun `공백이 아니고 비번과 비번 확인이 같다`() {
+        // given
+        val password = Password(
+            password = "123",
+            passwordConfirm = "123",
+        )
+
+        // then
+        password.isValid() shouldBe true
+    }
+
+}
