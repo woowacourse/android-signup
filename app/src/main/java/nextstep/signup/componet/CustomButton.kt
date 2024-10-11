b16bd81b356e8c98e813a68c214a2b0e02edec9b
JunJangE <wnswkd486@gmail.com>
2024-10-11 16:01:37 +0900

    [에디] 회원가입 미션 1, 2단계 제출합니다. (#4)
    * docs: 기능 요구 사항 정의
    
    * test: LayoutBasicsTest
    
    * test: text() 테스트를 성공하도록 수정
    
    * test: column() 테스트를 성공하도록 수정
    
    * test: button() 테스트를 성공하도록 수정
    
    * feat: ui 패키지 구조 변경
    
    * style: ktlint
    
    * feat: CustomButton
    
    * feat: CustomText
    
    * feat: CustomTextField
    
    * feat: 회원가입 뷰
    
    * refactor: InputField에서 MutableState 프로퍼티 제거
    
    * docs: step1 기능 요구 사항 재정의
    
    * test: Preview 학습 테스트
    
    * style: ktlint
    
    * chore: 필요없는 주석 제거
    
    * test: Button 테스트 활성화 기준 변경
    
    * refactor: 테스트 코드 상수화
    
    * refactor: textField 로직 개선

---
+++ app/src/main/java/nextstep/signup/componet/CustomButton.kt
@@ -0,0 +1,54 @@
+package nextstep.signup.componet
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.signup.R
+import nextstep.signup.ui.theme.Blue50
+
+@Composable
+fun CustomButton(
+    modifier: Modifier = Modifier,
+    shape: RoundedCornerShape = RoundedCornerShape(8.dp),
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(),
+    @StringRes titleResId: Int,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick,
+        shape = shape,
+        colors = buttonColors,
+    ) {
+        Text(
+            text = stringResource(id = titleResId),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.W500,
+            modifier = Modifier.padding(vertical = 15.dp),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CustomButtonPreView() {
+    CustomButton(
+        modifier = Modifier.padding(horizontal = 32.dp),
+        shape = RoundedCornerShape(100.dp),
+        buttonColors = ButtonDefaults.buttonColors(containerColor = Blue50),
+        titleResId = R.string.sign_up_button,
+        onClick = {},
+    )
+}
