5652aaee0982c7e00d53018defedb7a086925c0b
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-13 08:28:40 +0900

    test: 테스트 코드에 Preview 추가

--- app/src/androidTest/java/nextstep/signup/LayoutBasicTest.kt
+++ app/src/androidTest/java/nextstep/signup/LayoutBasicTest.kt
@@ -3,6 +3,7 @@ package nextstep.signup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -22,6 +23,7 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import org.junit.Rule
 import org.junit.Test
@@ -35,15 +37,7 @@ class LayoutBasicTest {
         // given
         val text = "안녕 난 컴포즈야~"
         composeTestRule.setContent {
-            Text(
-                text = "안녕 난 컴포즈야~",
-                color = Color.Blue,
-                style = TextStyle(
-                    fontSize = 26.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.SansSerif
-                )
-            )
+            IntroduceText(text)
         }
 
         // then
@@ -56,13 +50,7 @@ class LayoutBasicTest {
     fun column() {
         // given
         composeTestRule.setContent {
-            Column(
-                modifier = Modifier.testTag("이름")
-            ) {
-                Text(text = "깜포즈", color = Color.Red)
-                Text(text = "킴포즈", color = Color.Cyan)
-                Text(text = "끔포즈", color = Color.Yellow)
-            }
+            ComposeNamesList()
         }
 
         composeTestRule.onNodeWithTag("이름")
@@ -76,16 +64,7 @@ class LayoutBasicTest {
     fun button() {
         // given
         composeTestRule.setContent {
-            val enabled = remember { mutableStateOf(true) }
-            Button(
-                onClick = {
-                    enabled.value = !enabled.value
-                },
-                enabled = enabled.value,
-                modifier = Modifier.testTag("버튼")
-            ) {
-                Text(text = "클릭해주세요")
-            }
+            ClickableButton()
         }
 
         // when
@@ -95,5 +74,46 @@ class LayoutBasicTest {
 
         // then
         button.assertIsNotEnabled()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun IntroduceText(text: String = "안녕 난 컴포즈야~") {
+    Text(
+        text = text,
+        color = Color.Blue,
+        style = TextStyle(
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif
+        )
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ComposeNamesList() {
+    Column(
+        modifier = Modifier.testTag("이름")
+    ) {
+        Text(text = "깜포즈", color = Color.Red)
+        Text(text = "킴포즈", color = Color.Cyan)
+        Text(text = "끔포즈", color = Color.Yellow)
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ClickableButton() {
+    val enabled = remember { mutableStateOf(true) }
+    Button(
+        onClick = {
+            enabled.value = !enabled.value
+        },
+        enabled = enabled.value,
+        modifier = Modifier.testTag("버튼")
+    ) {
+        Text(text = "클릭해주세요")
     }
 }
