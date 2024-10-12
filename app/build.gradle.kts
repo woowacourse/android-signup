a02f66fc6cd0389da84d6e258c4a5ada021c64f9
Hogu59 <dbstjdguswkd@naver.com>
2024-10-12 23:59:02 +0900

    remove(gradle): erase duplicated dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    // androidx.compose.material3:material3
-    implementation(libs.androidx.material3)
 }
