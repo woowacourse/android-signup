9e5a65629f0f7d55a4527e23f8e475731f81cdb6
aprilgom <aprilgom@gmail.com>
2024-10-12 14:05:58 +0900

    refactor: TextField 간 spacedBy 활용

--- app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/signup/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package nextstep.signup.ui.signup
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,10 +19,13 @@ import nextstep.signup.R
 import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
-fun SignUpScreen(modifier: Modifier = Modifier) {
+fun SignUpScreen(
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(42.dp),
     ) {
         val userName = remember { mutableStateOf("") }
         val email = remember { mutableStateOf("") }
@@ -37,35 +41,34 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
                     isPasswordSame
 
         Text(
-            modifier = Modifier.padding(bottom = 42.dp),
             text = stringResource(id = R.string.welcome),
             fontSize = 26.sp,
             fontWeight = FontWeight.Bold,
         )
-        SignUpTextField(
-            modifier = Modifier.padding(bottom = 36.dp),
-            label = stringResource(id = R.string.user_name),
-            text = userName,
-            onValueChange = { userName.value = it },
-        )
-        EmailTextField(
-            modifier = Modifier.padding(bottom = 36.dp),
-            label = stringResource(id = R.string.email),
-            text = email,
-            onValueChange = { email.value = it },
-        )
-        PasswordTextField(
-            modifier = Modifier.padding(bottom = 36.dp),
-            label = stringResource(id = R.string.password),
-            text = password,
-            onValueChange = { password.value = it },
-        )
-        PasswordTextField(
-            modifier = Modifier.padding(bottom = 42.dp),
-            label = stringResource(id = R.string.password_confirm),
-            text = passwordConfirm,
-            onValueChange = { passwordConfirm.value = it },
-        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(36.dp),
+        ) {
+            SignUpTextField(
+                label = stringResource(id = R.string.user_name),
+                text = userName,
+                onValueChange = { userName.value = it },
+            )
+            EmailTextField(
+                label = stringResource(id = R.string.email),
+                text = email,
+                onValueChange = { email.value = it },
+            )
+            PasswordTextField(
+                label = stringResource(id = R.string.password),
+                text = password,
+                onValueChange = { password.value = it },
+            )
+            PasswordTextField(
+                label = stringResource(id = R.string.password_confirm),
+                text = passwordConfirm,
+                onValueChange = { passwordConfirm.value = it },
+            )
+        }
         ConfirmButton(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
