17f3e0d412257fdc3cc56a8dd3a0ccb2adf4db75
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-10-14 12:47:55 +0900

    feat: 컴포즈 학습 테스트 코드 수정 및 완성

--- app/src/androidTest/java/nextstep/signup/LayoutBasicsTest.kt
+++ app/src/androidTest/java/nextstep/signup/LayoutBasicsTest.kt
@@ -1,8 +1,10 @@
 package nextstep.signup
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -10,6 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -21,26 +24,23 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.junit.Rule
 import org.junit.Test
 
-// 1. 모든 테스트가 성공하도록 만들어보자
-// 2. 힌트를 참고하여 Preview를 노출시킨다.
-// 3. Preview의 interactive 모드를 활용하여 버튼을 클릭해본다.
 class LayoutBasicsTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
-    fun text() {
+    fun textTest() {
         // given
-        val text = "안녕 난 컴포즈야~"
         composeTestRule.setContent {
             Text(
-                // 바꿔 보세요!
-                text = "텍스트",
+                text = DISPLAY_TEXT,
                 color = Color.Blue,
                 style = TextStyle(
                     fontSize = 26.sp,
@@ -52,20 +52,27 @@ class LayoutBasicsTest {
 
         // then
         composeTestRule
-            .onNodeWithText(text)
+            .onNodeWithText(DISPLAY_TEXT)
             .assertExists()
     }
 
+    @Preview(showBackground = true)
+    @Composable
+    private fun previewText() {
+        textTest()
+    }
+
+
     @Test
-    fun column() {
+    fun columnTest() {
         // given
         composeTestRule.setContent {
             Column(
                 modifier = Modifier.testTag("이름")
             ) {
-                // 바꿔 보세요!
-                Text(text = "킴포즈", color = Color.Cyan)
-                Text(text = "끔포즈", color = Color.Yellow)
+                Text(text = names[0], color = Color.Cyan)
+                Text(text = names[1], color = Color.Yellow)
+                Text(text = names[2], color = Color.Green)
             }
         }
 
@@ -74,11 +81,11 @@ class LayoutBasicsTest {
             .onChildren()
             .assertCountEquals(3)
             .onFirst()
-            .assert(hasText("깜포즈"))
+            .assert(hasText(names[0]))
     }
 
     @Test
-    fun button() {
+    fun buttonTest() {
         // given
         composeTestRule.setContent {
             val enabled = remember { mutableStateOf(true) }
@@ -87,18 +94,23 @@ class LayoutBasicsTest {
                     enabled.value = false
                 },
                 enabled = enabled.value,
-                modifier = Modifier.testTag("버튼")
+                modifier = Modifier
+                    .testTag(BUTTON)
+                    .padding(16.dp)
             ) {
-                Text(text = "클릭해주세요")
+                Text(text = "클릭해줘!")
             }
         }
 
         // when
-        val button = composeTestRule
-            .onNodeWithTag("버튼")
+        composeTestRule
+            .onNodeWithTag(BUTTON)
+            .assertIsEnabled()
             .performClick()
 
         // then
-        button.assertIsNotEnabled()
+        composeTestRule
+            .onNodeWithTag(BUTTON)
+            .assertIsNotEnabled()
     }
 }
--- app/src/androidTest/java/nextstep/signup/TestFixture.kt
+++ app/src/androidTest/java/nextstep/signup/TestFixture.kt
@@ -1,2 +1,5 @@
 package nextstep.signup
 
+const val DISPLAY_TEXT = "안녕 난 컴포즈야~"
+val names = listOf("채채", "체체", "첵첵")
+const val BUTTON = "버튼"
