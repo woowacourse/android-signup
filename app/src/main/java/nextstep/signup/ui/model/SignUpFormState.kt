1b8476d0151983ca8b1c02ebdbf395920b723abe
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-14 20:29:27 +0900

    feat: 회원가입 폼 객체에 유효성 검사 로직 추가

--- app/src/main/java/nextstep/signup/ui/model/SignUpFormState.kt
+++ app/src/main/java/nextstep/signup/ui/model/SignUpFormState.kt
@@ -1,7 +1,9 @@
 package nextstep.signup.ui.model
 
 import android.os.Parcelable
+import androidx.annotation.StringRes
 import kotlinx.parcelize.Parcelize
+import nextstep.signup.R
 
 @Parcelize
 data class SignUpFormState(
@@ -9,4 +11,48 @@ data class SignUpFormState(
     val email: String = "",
     val password: String = "",
     val passwordConfirmation: String = "",
-) : Parcelable
+    val completed: Boolean = false,
+) : Parcelable {
+    val usernameStatus: SignUpStatus
+        get() =
+            when {
+                username.isEmpty() -> SignUpStatus.Empty
+                username.length !in 2..5 -> SignUpStatus.Error.UsernameLength
+                username.matches(Regex(USERNAME_REGEX)) -> SignUpStatus.Success
+                else -> SignUpStatus.Error.UsernameNonCharacter
+            }
+
+    val emailStatus: SignUpStatus
+        get() =
+            when {
+                email.isEmpty() -> SignUpStatus.Empty
+                email.matches(Regex(EMAIL_REGEX)) -> SignUpStatus.Success
+                else -> SignUpStatus.Error.EmailFormat
+            }
+
+    val passwordStatus: SignUpStatus
+        get() =
+            when {
+                password.isEmpty() -> SignUpStatus.Empty
+                password.length !in 8..16 -> SignUpStatus.Error.PasswordLength
+                password.matches(Regex(PASSWORD_REGEX)) -> SignUpStatus.Success
+                else -> SignUpStatus.Error.PasswordFormat
+            }
+
+    val passwordConfirmationStatus: SignUpStatus
+        get() =
+            when {
+                passwordConfirmation.isEmpty() -> SignUpStatus.Empty
+                password == passwordConfirmation -> SignUpStatus.Success
+                else -> SignUpStatus.Error.PasswordConfirmation
+            }
+
+    val formValid: Boolean
+        get() = usernameStatus == null && emailStatus == null && passwordStatus == null && passwordConfirmationStatus == null
+
+    companion object {
+        private const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
+        private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+        private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+    }
+}
