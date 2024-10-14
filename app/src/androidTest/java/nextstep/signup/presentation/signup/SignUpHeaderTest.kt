ddebcb5b55c2f5366bf7f2dc7ef5b632cc35018e
sh1mj1 <7wlgns@gmail.com>
2024-10-14 16:24:15 +0900

    chore(SignUpHeaderTest): 패키지 이동

--- app/src/androidTest/java/nextstep/signup/presentation/signup/SignUpHeaderTest.kt
+++ app/src/androidTest/java/nextstep/signup/presentation/signup/SignUpHeaderTest.kt
@@ -1,8 +1,7 @@
-package nextstep.signup
+package nextstep.signup.presentation.signup
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import nextstep.signup.presentation.signup.SignUpHeader
 import org.junit.Rule
 import org.junit.Test
 
