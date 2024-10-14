c9f27cb09506de2de489310d21c1af68eccc1ac3
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-14 14:50:36 +0900

    [해음] 회원가입 미션 1, 2단계 제출합니다. (#18)
    * docs: step1 테스트 목록표 작성
    
    * test: LayoutBasicTest 학습 테스트 완료
    
    * docs: 기능 요구 사항, 프로그래밍 요구 사항 업데이트
    
    * feat: 환영 문구 텍스트
    
    * feat: 이름 입력 텍스트 필드
    
    * feat: 이메일 입력 텍스트 필드
    
    * feat: 비밀번호 입력 텍스트 필드
    
    * feat: 비밀번호 확인 입력 텍스트 필드
    
    * feat: 회원가입 버튼
    
    * refactor: getString -> stringResource 변경
    
    * docs: 완료한 기능 목록 체크
    
    * refactor: Spacer -> Arrangement.spacedBy 사용으로 변경
    
    * config: ktlint 의존성 추가
    
    * style: ktlint
    
    * test: 테스트 코드에 Preview 추가
    
    * style: ktlint
    
    * fix: assert 호출부 하드코딩으로 변경
    
    * refactor: height -> contentPadding 사용으로 변경
    
    * refactor: SignUpButton signUp 매개변수로 받게 변경
    
    * refactor: 다양한 곳에서 사용할 수 있게 Default prefix 추가
    
    * refactor: 컴포넌트 분리
    
    * refactor: Surface 패딩 -> Column 패딩으로 변경
    
    * style: ktlint

---
+++ app/src/main/java/nextstep/signup/Headlines.kt
@@ -0,0 +1,22 @@
+package nextstep.signup
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HeadLine(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = Color.Black,
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
--- app/src/main/java/nextstep/signup/MainActivity.kt
+++ app/src/main/java/nextstep/signup/MainActivity.kt
@@ -3,13 +3,24 @@ package nextstep.signup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import nextstep.signup.ui.theme.SignupTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +28,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SignupTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surface
                 ) {
-                    Greeting("Android")
+                    SignUpScreen()
                 }
             }
         }
@@ -30,17 +41,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SignUpScreen() {
+    var username by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordConfirm by rememberSaveable { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SignupTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.padding(
+            start = 32.dp,
+            top = 50.dp,
+            end = 32.dp
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(36.dp)
+    ) {
+        HeadLine(text = stringResource(R.string.sign_up_headline))
+        PlainTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = stringResource(id = R.string.sign_up_username_label)
+        )
+        EmailTextField(
+            email = email,
+            onValueChange = { email = it },
+            label = stringResource(R.string.sign_up_email_label)
+        )
+        PasswordTextField(
+            password = password,
+            onValueChange = { password = it },
+            label = stringResource(R.string.sign_up_password_label)
+        )
+        PasswordTextField(
+            password = passwordConfirm,
+            onValueChange = { passwordConfirm = it },
+            label = stringResource(R.string.sign_up_password_confirm_label)
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        DefaultButton(contentPadding = PaddingValues(15.dp)) {}
     }
 }
