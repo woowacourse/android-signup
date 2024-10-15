3fc55c849c157481b6464fc5cdab1166fbb7ee67
kkosang <eiwoqp77@gmail.com>
2024-10-15 10:48:20 +0900

    refactor: SignUpButton color 변경

--- app/src/main/java/nextstep/signup/component/SignUpSubmitButton.kt
+++ app/src/main/java/nextstep/signup/component/SignUpSubmitButton.kt
@@ -6,15 +6,16 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import nextstep.signup.R
 
 @Composable
 fun SignUpSubmitButton(modifier: Modifier, text: String, onClick: () -> Unit) {
     Button(
         modifier = modifier,
         onClick = { onClick() },
-        colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray),
+        colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.blue_50)),
         contentPadding = PaddingValues(vertical = 15.dp)
     ) {
         Text(text = text)
