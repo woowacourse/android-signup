5a9915822c08729bfd3db17c25ba197ff8d55144
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-15 20:29:56 +0900

    refactor: 회원가입 버튼 테스트 객체명 변경

--- app/src/androidTest/java/nextstep/signup/ui/ButtonOnSignUpValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/ui/ButtonOnSignUpValidationTest.kt
@@ -11,7 +11,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class SignUpButtonValidationTest {
+class ButtonOnSignUpValidationTest {
     @get:Rule
     val composeTestRule = createComposeRule()
     private val userForm = mutableStateOf(UserForm())
