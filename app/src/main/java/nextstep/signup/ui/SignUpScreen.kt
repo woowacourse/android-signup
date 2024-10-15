f0a02aff1304435e12fd4b08e7bc7330d945d319
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-15 19:19:57 +0900

    feat: 회원가입 완료 시 스낵바 표출 기능 추가

--- app/src/main/java/nextstep/signup/ui/SignUpScreen.kt
+++ app/src/main/java/nextstep/signup/ui/SignUpScreen.kt
@@ -3,21 +3,36 @@ package nextstep.signup.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
+import nextstep.signup.R
 import nextstep.signup.ui.model.SignUpFormState
 
 @Composable
 fun SignUpScreen(modifier: Modifier = Modifier) {
     var signUpForm: SignUpFormState by rememberSaveable { mutableStateOf(SignUpFormState()) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
+        snackbarHost = {
+            SnackbarHost(snackbarHostState)
+        }
     ) { paddingValues ->
         SignUpForm(
             modifier =
@@ -28,7 +43,14 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
             onSignUpFormChange = { changedValue ->
                 signUpForm = changedValue.copy(completed = changedValue.formValid)
             },
-            onConfirm = {},
+            onConfirm = {
+                scope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = context.getString(R.string.signup_complete_message),
+                        duration = SnackbarDuration.Short,
+                    )
+                }
+            },
         )
     }
 }
