261df60fe26ecbc18784420ec964bea72872e48f
kkosang <eiwoqp77@gmail.com>
2024-10-16 01:07:23 +0900

    feat: Username 유효성 검사 구현

--- app/src/main/java/nextstep/signup/component/SignUpUsernameTextField.kt
+++ app/src/main/java/nextstep/signup/component/SignUpUsernameTextField.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import nextstep.signup.R
+import nextstep.signup.domain.validation.Username
+import nextstep.signup.domain.validation.ValidationResult
 
 @Composable
 internal fun SignUpUsernameTextField(
@@ -13,11 +15,19 @@ internal fun SignUpUsernameTextField(
     username: String,
     onUsernameChange: (String) -> Unit,
 ) {
+    val validationResult = Username(username).validationResult()
+    val supportingText = when (validationResult) {
+        ValidationResult.INVALID_LENGTH -> stringResource(R.string.error_name_length)
+        ValidationResult.INVALID_FORMAT -> stringResource(R.string.error_name_format)
+        else -> ""
+    }
+
     TextField(
         modifier = modifier,
         value = username,
         onValueChange = { onUsernameChange(it) },
         label = { Text(stringResource(R.string.input_hint_username)) },
-        supportingText = { },
+        supportingText = { if (supportingText.isNotEmpty()) Text(text = supportingText) },
+        isError = supportingText.isNotEmpty()
     )
 }
---
+++ app/src/main/java/nextstep/signup/domain/validation/Username.kt
@@ -0,0 +1,22 @@
+package nextstep.signup.domain.validation
+
+data class Username(private val name: String = "") {
+    private val regex = Regex(USERNAME_REGEX)
+
+    fun validationResult(): ValidationResult {
+        return when {
+            name.isEmpty() -> ValidationResult.EMPTY
+            !isValidLength() -> ValidationResult.INVALID_LENGTH
+            !isValidFormat() -> ValidationResult.INVALID_FORMAT
+            else -> ValidationResult.SUCCESS
+        }
+    }
+
+    private fun isValidLength(): Boolean = name.length in 2..5
+
+    private fun isValidFormat(): Boolean = name.matches(regex)
+
+    companion object {
+        const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
+    }
+}
