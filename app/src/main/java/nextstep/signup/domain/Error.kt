f9920a3efae8dc81f5f262ccd4cb1e23b5d508d8
Hogu59 <dbstjdguswkd@naver.com>
2024-10-15 14:37:27 +0900

    fix: fix error message bug

--- app/src/main/java/nextstep/signup/domain/Error.kt
+++ app/src/main/java/nextstep/signup/domain/Error.kt
@@ -1,9 +1,7 @@
 package nextstep.signup.domain
 
-import nextstep.signup.R
-
 enum class Error(val code : Int, val message : String) {
-    INVALID_USERNAME_LENGTH(100, R.string.sign_up_form_username_error.toString()),
+    INVALID_USERNAME_LENGTH(100, "이름은 2자 이상 5자 이하이여야 합니다."),
     INVALID_USERNAME_TYPE(101, "이름에는 숫자나 기호가 포함될 수 없습니다."),
     INVALID_EMAIL(200, "이메일 형식이 올바르지 않습니다."),
     INVALID_PASSWORD_LENGTH(300, "비밀번호는 8자 이상 16자 이하이어야 합니다."),
