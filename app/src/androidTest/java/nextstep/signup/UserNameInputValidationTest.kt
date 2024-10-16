8d456f0f0f683deebc04950450ac05ddf164ac07
Namyunsuk <yunsuk0328@naver.com>
2024-10-16 11:42:24 +0900

    refactor: 테스트 패키지 분리

--- app/src/androidTest/java/nextstep/signup/UserNameInputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/UserNameInputValidationTest.kt
@@ -3,6 +3,7 @@ package nextstep.signup
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import nextstep.signup.fixtures.FakeSignUpField
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
---
+++ app/src/androidTest/java/nextstep/signup/fixtures/TestFixture.kt
@@ -0,0 +1,23 @@
+package nextstep.signup.fixtures
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import nextstep.signup.ValidationResult
+
+@Composable
+fun FakeSignUpField(
+    value: String,
+    validationResult: ValidationResult,
+    onValueChange: (String) -> Unit,
+) {
+    TextField(
+        value = value,
+        onValueChange = { onValueChange(it) },
+        supportingText = {
+            Text(
+                text = validationResult.errorMessage,
+            )
+        },
+    )
+}
