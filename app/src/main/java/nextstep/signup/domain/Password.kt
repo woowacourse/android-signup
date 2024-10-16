e73ec324b568a89d3fd0971e77ff8b41d1eeb49e
sh1mj1 <7wlgns@gmail.com>
2024-10-16 16:36:54 +0900

    feat(Password): 패스워드 길이 유효성

--- app/src/main/java/nextstep/signup/domain/Password.kt
+++ app/src/main/java/nextstep/signup/domain/Password.kt
@@ -1,8 +1,19 @@
 package nextstep.signup.domain
 
+
 data class Password(
-    val password: String,
-    val passwordConfirm: String
+    val password: String = "",
+    val passwordConfirm: String = "",
 ) {
+    init {
+        require(password.length in PASSWORD_RANGE) { "password has to be in $PASSWORD_RANGE" }
+    }
+
     fun isValid(): Boolean = password.isNotBlank() && password == passwordConfirm
+
+    companion object {
+        private const val MIN_PASSWORD_LENGTH = 8
+        private const val MAX_PASSWORD_LENGTH = 16
+        private val PASSWORD_RANGE = MIN_PASSWORD_LENGTH..MAX_PASSWORD_LENGTH
+    }
 }
--- app/src/test/java/nextstep/signup/domain/PasswordTest.kt
+++ app/src/test/java/nextstep/signup/domain/PasswordTest.kt
@@ -1,30 +1,27 @@
 package nextstep.signup.domain
 
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PasswordTest {
-    @Test
-    fun `공백이면 유효하지 않은 패스워드`() {
-        // given
-        val password = Password(
-            password = "",
-            passwordConfirm = ""
-        )
-
-        // then
-        password.isValid() shouldBe false
+    @ParameterizedTest
+    @ValueSource(strings = ["t", "1", "123", "fm298"])
+    fun `패스워드의 길이가 8자 미만이면 유효하지 않다`(password: String) {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "password has to be in 8..16") {
+            Password(password = password)
+        }
     }
 
-    @Test
-    fun `공백이 아니고 비번과 비번 확인이 같다`() {
-        // given
-        val password = Password(
-            password = "123",
-            passwordConfirm = "123"
-        )
-
-        // then
-        password.isValid() shouldBe true
+    @ParameterizedTest
+    @ValueSource(strings = ["148jmdshfi289sdf9", "118283487591345123", "dsfkjshfi2y834234"])
+    fun `패스워드의 길이가 16자 초과이면 유효하지 않다`(password: String) {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "password has to be in 8..16") {
+            Password(password = password)
+        }
     }
+
+
 }
