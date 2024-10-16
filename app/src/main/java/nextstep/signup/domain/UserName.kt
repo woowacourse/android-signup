7de77f879b9c57ebb573fff4b45f11e1a051bbf3
sh1mj1 <7wlgns@gmail.com>
2024-10-16 16:29:58 +0900

    feat(UserName): 이름에는 숫자나 기호가 포함될 수 없습니다

--- app/src/main/java/nextstep/signup/domain/UserName.kt
+++ app/src/main/java/nextstep/signup/domain/UserName.kt
@@ -6,6 +6,7 @@ value class UserName(
 ) {
     init {
         require(name.length in NAME_RANGE) { "name has to be in $NAME_RANGE" }
+        require(name.matches(regex)) { "name must not include number or symbol" }
     }
 
     fun isValid(): Boolean = name.isNotBlank()
@@ -14,5 +15,8 @@ value class UserName(
         private const val MIN_NAME_LENGTH = 2
         private const val MAX_NAME_LENGTH = 5
         private val NAME_RANGE = MIN_NAME_LENGTH..MAX_NAME_LENGTH
+
+        private const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
+        private val regex = Regex(USERNAME_REGEX)
     }
 }
--- app/src/test/java/nextstep/signup/domain/UserNameTest.kt
+++ app/src/test/java/nextstep/signup/domain/UserNameTest.kt
@@ -1,28 +1,39 @@
 package nextstep.signup.domain
 
 import io.kotest.assertions.throwables.shouldNotThrow
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.throwables.shouldThrowWithMessage
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class UserNameTest {
     @ParameterizedTest
-    @ValueSource(strings = ["12", "abc", "sdfs", "sdfsf"])
-    fun `이름의 길이가 2자 이상 5자 이하이면 유효하다`(name: String) {
-        shouldNotThrow<IllegalArgumentException> {
+    @ValueSource(strings = ["t", "a", "sdfsfeie", "fkskdie"])
+    fun `이름의 길이가 2자 미만 5자 초과이면 유효하지 않다`(name: String) {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "name has to be in 2..5") {
             UserName(name)
         }
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["1", "a", "sdfsfeie", "fkskdie"])
-    fun `이름의 길이가 2자 미만 5자 초과이면 유효 하지 않다`(name: String) {
-        shouldThrow<IllegalArgumentException> {
+    @ValueSource(strings = ["11", "a1", "2sf", "0sd9"])
+    fun `이름에 숫자가 들어가면 유효하지 않다`(name: String) {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "name must not include number or symbol") {
             UserName(name)
         }
-        shouldThrowWithMessage<IllegalArgumentException>(message = "name has to be in 2..5") {
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["++", "-_", "$5", "^tt^"])
+    fun `이름에 기호가 들어가면 유효하지 않다`(name: String) {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "name must not include number or symbol") {
+            UserName(name)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["ab", "abc", "sdfs", "sdfsf"])
+    fun `이름의 길이가 2자 이상 5자 이하이며 이름에 기호와 숫자가 들어가지 않으면 유효하다`(name: String) {
+        shouldNotThrow<IllegalArgumentException> {
             UserName(name)
         }
     }
