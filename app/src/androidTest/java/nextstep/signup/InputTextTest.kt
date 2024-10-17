9991a433982f3d17724898ce764409d8018f574b
JoYehyun <jo990714@naver.com>
2024-10-17 16:10:40 +0900

    chore: 테스트 네이밍 수정

--- app/src/androidTest/java/nextstep/signup/InputTextTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputTextTest.kt
@@ -11,7 +11,7 @@ import nextstep.signup.ui.component.InputText
 import org.junit.Rule
 import org.junit.Test
 
-class InputValidationTest {
+class InputTextTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
