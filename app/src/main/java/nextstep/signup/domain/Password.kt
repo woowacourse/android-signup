b8919c5bf58c9b4775c539f917443df1f2a3db5c
sh1mj1 <7wlgns@gmail.com>
2024-10-17 20:57:35 +0900

    feat: Password 팩토리 함수, PasswordResult

--- app/src/main/java/nextstep/signup/domain/Password.kt
+++ app/src/main/java/nextstep/signup/domain/Password.kt
@@ -20,5 +20,27 @@ data class Password(
         private const val PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d).+$"
 
         private val regex = Regex(PASSWORD_REGEX)
+
+        fun from(password: String, passwordConfirm: String): PasswordResult {
+            if (password == "" || passwordConfirm == "") return PasswordResult.EmptyField
+            if (password.length !in PASSWORD_RANGE) return PasswordResult.InvalidPasswordLength
+            if (!regex.matches(password)) return PasswordResult.InvalidPasswordFormat
+            if (password != passwordConfirm) return PasswordResult.NotSamePasswordConfirm
+            return PasswordResult.Success(password, passwordConfirm)
+        }
     }
+}
+
+sealed interface PasswordResult {
+    data class Success(val password: String, val passwordConfirm: String) : PasswordResult
+
+    data object EmptyField : PasswordResult
+
+    sealed interface Failure : PasswordResult
+
+    data object InvalidPasswordLength : Failure
+
+    data object InvalidPasswordFormat : Failure
+
+    data object NotSamePasswordConfirm : Failure
 }
