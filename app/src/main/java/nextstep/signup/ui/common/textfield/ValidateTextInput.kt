38ff5ea8fd3e3c05d2dcf4d76404888dc2986c74
Junyoung-WON <wjyo60@gmail.com>
2024-10-18 13:49:40 +0900

    feat: (3단계) 이메일 검증 로직 구현

--- app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
+++ app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
@@ -1,15 +1,27 @@
 package nextstep.signup.ui.common.textfield
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+
 const val USERNAME_REGAX = "^[a-zA-Z가-힣]+$"
+const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
 
 fun validateUsernameInput(input: String): String {
-    val nameRegex = USERNAME_REGAX.toRegex()
     return if (input.isEmpty()) {
         ""
-    } else if (!nameRegex.matches(input)) {
+    } else if (!input.matches(Regex(USERNAME_REGAX))) {
         "이름에는 숫자나 기호가 포함될 수 없습니다."
     } else if (input.length !in 2..5) {
         "이름은 2~5자여야 합니다."
+    } else {
+        ""
+    }
+}
+
+fun validateEmailInput(input: String): String {
+    return if (input.isEmpty()) {
+        ""
+    } else if (!input.matches(Regex(EMAIL_REGEX))) {
+        "이메일 형식이 올바르지 않습니다."
     } else {
         ""
     }
