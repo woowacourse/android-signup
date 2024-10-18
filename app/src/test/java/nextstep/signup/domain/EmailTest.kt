81c7a8ca38e251220f265c07fff2d795aebf0dc3
sh1mj1 <7wlgns@gmail.com>
2024-10-18 09:50:12 +0900

    test: UserNameTest from 메서드

--- app/src/test/java/nextstep/signup/domain/EmailTest.kt
+++ app/src/test/java/nextstep/signup/domain/EmailTest.kt
@@ -7,6 +7,27 @@ import org.junit.jupiter.api.Test
 
 class EmailTest {
     @Test
+    fun `이메일 형식에 맞도록 입력한 경우 유효하다`() {
+        shouldNotThrow<IllegalArgumentException> {
+            Email("sh1mj1@wooteco.com")
+        }
+    }
+
+    @Test
+    fun `이메일 입력에 @가 없으면 유효하지 않다`() {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "the email format is not correct") {
+            Email("sh1mj1")
+        }
+    }
+
+    @Test
+    fun `이메일 입력 @ 뒤에 점이 없으며 유효하지 않다`() {
+        shouldThrowWithMessage<IllegalArgumentException>(message = "the email format is not correct") {
+            Email("sh1mj1@wootecocom")
+        }
+    }
+
+    @Test
     fun `입력이 이메일 형식에 맞는 경우 성공 이메일 결과가 리턴된다`() {
         // given & when
         val input = "sh1mj1@wooteco.com"
@@ -32,26 +53,5 @@ class EmailTest {
 
         // then
         emailResult shouldBe EmailResult.InvalidNameFormat
-    }
-
-    @Test
-    fun `이메일 형식에 맞도록 입력한 경우 유효하다`() {
-        shouldNotThrow<IllegalArgumentException> {
-            Email("sh1mj1@wooteco.com")
-        }
-    }
-
-    @Test
-    fun `이메일 입력에 @가 없으면 유효하지 않다`() {
-        shouldThrowWithMessage<IllegalArgumentException>(message = "the email format is not correct") {
-            Email("sh1mj1")
-        }
-    }
-
-    @Test
-    fun `이메일 입력 @ 뒤에 점이 없으며 유효하지 않다`() {
-        shouldThrowWithMessage<IllegalArgumentException>(message = "the email format is not correct") {
-            Email("sh1mj1@wootecocom")
-        }
     }
 }
--- app/src/test/java/nextstep/signup/domain/UserNameTest.kt
+++ app/src/test/java/nextstep/signup/domain/UserNameTest.kt
@@ -2,6 +2,7 @@ package nextstep.signup.domain
 
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -37,4 +38,35 @@ class UserNameTest {
             UserName(name)
         }
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["t", "a", "sdfsfeie", "fkskdie"])
+    fun `이름 입력이 2자 미만 5자 초과이면 유효하지 않은 길이의 사용자 이름 결과가 리턴된다`(input: String) {
+        // given & when
+        val userNameResult = UserName.from(input = input)
+
+        // then
+        userNameResult shouldBe UserNameResult.InvalidNameLength
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["11", "a1", "2sf", "0sd9", "++", "-_", "$5", "^tt^"])
+    fun `이름 입력에 숫자 혹은 기호가 들어가면 유효하지 않은 형식의 사용자 이름 결과가 리턴된다`(input: String) {
+        // given & when
+        val userNameResult = UserName.from(input = input)
+
+        // then
+        userNameResult shouldBe UserNameResult.InvalidNameFormat
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["ab", "abc", "sdfs", "sdfsf"])
+    fun `이름 입력에 2자 이상 5자 이하이며 이름에 기호와 숫자가 들어가지 않으면 성공 사용자 이름 결과가 리턴된다`(input: String) {
+        // given & when
+        val userNameResult = UserName.from(input = input)
+
+        // then
+        userNameResult shouldBe UserNameResult.Success(UserName(input))
+    }
+
 }
