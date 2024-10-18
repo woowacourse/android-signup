00f92e60a448e82771cdf7b815dbc8a5afa31fae
Junyoung-WON <wjyo60@gmail.com>
2024-10-18 15:01:43 +0900

    feat: (3단계) 비밀번호 형식 검증 로직 수정
    - 입력 길이에 대한 검증은 따로 수행하고 있으므로, 정규식에 포함된 입력 길이 조건을 제거한다.

--- app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
+++ app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
@@ -4,7 +4,7 @@ import android.provider.ContactsContract.CommonDataKinds.Email
 
 const val USERNAME_REGAX = "^[a-zA-Z가-힣]+$"
 const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
-const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).*$"
 
 fun validateUsernameInput(input: String): String {
     return if (input.isEmpty()) {
