80c329d3be3ce2d93a8cc0be71c5c859952c6d43
Junyoung-WON <wjyo60@gmail.com>
2024-10-18 14:10:34 +0900

    feat: (3단계) 비밀번호 검증 로직 구현
    - 8~16자 이내
    - 영문과 숫자 포함
    - 비밀번호 확인 입력은 비밀번호와 동일해야함

--- app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
+++ app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
@@ -4,6 +4,7 @@ import android.provider.ContactsContract.CommonDataKinds.Email
 
 const val USERNAME_REGAX = "^[a-zA-Z가-힣]+$"
 const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
 
 fun validateUsernameInput(input: String): String {
     return if (input.isEmpty()) {
@@ -22,6 +23,31 @@ fun validateEmailInput(input: String): String {
         ""
     } else if (!input.matches(Regex(EMAIL_REGEX))) {
         "이메일 형식이 올바르지 않습니다."
+    } else {
+        ""
+    }
+}
+
+fun validatePasswordInput(input: String): String {
+    return if (input.isEmpty()) {
+        ""
+    } else if (!input.matches(Regex(PASSWORD_REGEX))) {
+        "비밀번호는 영문과 숫자를 포함해야 합니다."
+    } else if (input.length !in 8..16) {
+        "비밀번호는 8~16자여야 합니다."
+    } else {
+        ""
+    }
+}
+
+fun validatePasswordConfirmInput(
+    password: String,
+    input: String,
+): String {
+    return if (input.isEmpty()) {
+        ""
+    } else if (input != password) {
+        "비밀번호가 일치하지 않습니다."
     } else {
         ""
     }
