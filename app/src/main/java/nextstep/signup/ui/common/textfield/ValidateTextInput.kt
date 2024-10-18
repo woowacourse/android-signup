029633cbde35e6bc4380bfbd34e2dfd56f34007e
Junyoung-WON <wjyo60@gmail.com>
2024-10-18 11:36:36 +0900

    feat: (3단계) 사용자 이름 입력에 대한 검증 로직 구현
    - 이름은 2~5자 이내여야 한다.
    - 이름에는 숫자나 기호가 포함될 수 없다: 정규식 활용

---
+++ app/src/main/java/nextstep/signup/ui/common/textfield/ValidateTextInput.kt
@@ -0,0 +1,14 @@
+package nextstep.signup.ui.common.textfield
+
+const val USERNAME_REGAX = "^[a-zA-Z가-힣]+$"
+
+fun validateUsernameInput(input: String): String {
+    val nameRegex = USERNAME_REGAX.toRegex()
+    return if (!nameRegex.matches(input)) {
+        "이름에는 숫자나 기호가 포함될 수 없습니다."
+    } else if (input.length !in 2..5 && input.isNotEmpty()) {
+        "이름은 2~5자여야 합니다."
+    } else {
+        ""
+    }
+}
