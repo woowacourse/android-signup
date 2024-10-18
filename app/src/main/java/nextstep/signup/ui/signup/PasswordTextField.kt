e9d707996aaa9e814a4575329f8ae544b23d1201
aprilgom <aprilgom@gmail.com>
2024-10-18 15:20:34 +0900

    feat: 비밀 번호의 형식을 지키지 않을 경우 에러 메시지가 노출된다

--- app/src/main/java/nextstep/signup/ui/signup/PasswordTextField.kt
+++ app/src/main/java/nextstep/signup/ui/signup/PasswordTextField.kt
@@ -5,8 +5,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.signup.R
+import nextstep.signup.model.CompositeValidation
+import nextstep.signup.model.LengthValidation
+import nextstep.signup.model.RegexValidation
 import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
@@ -15,13 +20,22 @@ fun PasswordTextField(
     label: String,
     text: MutableState<String>,
     onValueChange: (String) -> Unit = {},
-) = SignUpTextField(
-    label = label,
-    text = text,
-    onValueChange = onValueChange,
-    visualTransformation = PasswordVisualTransformation(),
-    modifier = modifier,
-)
+){
+
+    val lengthValidation = LengthValidation(8..16, stringResource(R.string.password_length_error))
+    val regex = "^(?=.*[a-zA-Z])(?=.*[0-9]).+\$".toRegex()
+    val regexValidation = RegexValidation(regex, stringResource(R.string.password_character_error))
+    val passwordValidation = CompositeValidation(lengthValidation, regexValidation)
+    SignUpTextField(
+        label = label,
+        text = text,
+        onValueChange = onValueChange,
+        visualTransformation = PasswordVisualTransformation(),
+        modifier = modifier,
+        isError = !passwordValidation.validate(text.value),
+        errorMessage = passwordValidation.errorMessage(text.value),
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -29,7 +43,7 @@ fun PasswordTextFieldPreview() {
     SignupTheme {
         PasswordTextField(
             label = "Password",
-            text = remember { mutableStateOf("") },
+            text = remember { mutableStateOf("aaaa12345") },
         )
     }
 }
