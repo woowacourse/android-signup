ff3166cf96435f37a3c970fb62b26bf47d41bce8
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-10-18 13:45:04 +0900

    feat: 비밀번호 유효성 검사 구현

---
+++ app/src/main/java/nextstep/signup/PasswordState.kt
@@ -0,0 +1,32 @@
+package nextstep.signup
+
+import nextstep.signup.InputValidationResult.Empty
+import nextstep.signup.InputValidationResult.LengthError
+import nextstep.signup.InputValidationResult.Valid
+import nextstep.signup.InputValidationResult.WrongFormat
+
+data class PasswordState(
+    val password: String = ""
+) {
+    fun validate(): InputValidationResult {
+        if (password.isEmpty()) {
+            return Empty
+        }
+
+        if (password.length !in PASSWORD_MINIMUM_LENGTH..PASSWORD_MAXIMUM_LENGTH) {
+            return LengthError
+        }
+
+        if (password.matches(Regex(PASSWORD_REGEX)).not()) {
+            return WrongFormat
+        }
+
+        return Valid
+    }
+
+    companion object {
+        private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+        private const val PASSWORD_MINIMUM_LENGTH = 8
+        private const val PASSWORD_MAXIMUM_LENGTH = 16
+    }
+}
