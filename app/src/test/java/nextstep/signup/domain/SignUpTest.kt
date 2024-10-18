843665b9f56fd351f0fe164bbfdef756c8889d98
sh1mj1 <7wlgns@gmail.com>
2024-10-18 10:36:18 +0900

    test: SignUpTest 유효한 회원가입

--- app/src/test/java/nextstep/signup/domain/SignUpTest.kt
+++ app/src/test/java/nextstep/signup/domain/SignUpTest.kt
@@ -1,28 +1,37 @@
 package nextstep.signup.domain
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
-// TODO: SignUp 으로
 class SignUpTest {
     @Test
     fun `유효한 회원가입 `() {
         // given
-        val userName = UserName(name = "심지")
-//        val mail = Email(
-//            id = EmailId("악어"),
-//            domain = EmailDomain("wooteco.com")
-//        )
-//        val password = Password3(
-//            password = "qwer",
-//            passwordConfirm = "qwer"
-//        )
-//        val signUp = SignUp3(
-//            userName,
-//            mail,
-//            password
-//        )
-//
-//        // then
-//        signUp.isValid() shouldBe true
+        val userNameInput = "qwer"
+        val emailInput = "qwer@wooteco.com"
+        val passwordInput = "qwer12345"
+        val passwordConfirmInput = "qwer12345"
+
+        // when
+        val signUpResult = SignUp.from(
+            userName = UserName.from(userNameInput),
+            email = Email.from(emailInput),
+            password = Password.from(passwordInput),
+            passwordConfirm = PasswordConfirm.from(
+                passwordInput,
+                passwordConfirmInput,
+            )
+        )
+
+        // then
+        signUpResult shouldBe SignUpResult.Success(
+            SignUp(
+                userName = UserName(userNameInput),
+                email = Email(emailInput),
+                password = Password(passwordInput),
+                passwordConfirm = PasswordConfirm(passwordConfirmInput)
+            )
+        )
+
     }
 }
