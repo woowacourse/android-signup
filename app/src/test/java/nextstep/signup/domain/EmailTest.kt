da68c8bdf49cb1cd4d30728e43f73a3203d42c3e
sh1mj1 <7wlgns@gmail.com>
2024-10-18 09:45:35 +0900

    test: EmailTest

--- app/src/test/java/nextstep/signup/domain/EmailTest.kt
+++ app/src/test/java/nextstep/signup/domain/EmailTest.kt
@@ -2,9 +2,38 @@ package nextstep.signup.domain
 
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class EmailTest {
+    @Test
+    fun `입력이 이메일 형식에 맞는 경우 성공 이메일 결과가 리턴된다`() {
+        // given & when
+        val input = "sh1mj1@wooteco.com"
+        val emailResult = Email.from(input = input)
+
+        // then
+        emailResult shouldBe EmailResult.Success(Email(input))
+    }
+
+    @Test
+    fun `입력이 공백인 경우 공백 이메일 결과가 리턴된다`() {
+        // given & when
+        val emailResult = Email.from(input = "")
+
+        // then
+        emailResult shouldBe EmailResult.EmptyField
+    }
+
+    @Test
+    fun `입력이 이메일 형식에 맞지 않은 경우 유효하지 않은 형식의 이메일 결과가 리턴된다`() {
+        // given & when
+        val emailResult = Email.from(input = "aa")
+
+        // then
+        emailResult shouldBe EmailResult.InvalidNameFormat
+    }
+
     @Test
     fun `이메일 형식에 맞도록 입력한 경우 유효하다`() {
         shouldNotThrow<IllegalArgumentException> {
