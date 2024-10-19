aea2eeb57dee51494826f9a236b98c12c26e7a95
somin <salth0601@gmail.com>
2024-10-19 17:56:54 +0900

    refactor: supportingText 반영

--- app/src/main/java/nextstep/signup/ui/SignUpTextField.kt
+++ app/src/main/java/nextstep/signup/ui/SignUpTextField.kt
@@ -2,7 +2,6 @@ package nextstep.signup.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -11,13 +10,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import nextstep.signup.ui.theme.Red
 import nextstep.signup.ui.theme.SignupTheme
 
 @Composable
@@ -40,25 +35,13 @@ fun SignUpTextField(
             placeholder = { Text(text = hint) },
             label = { Text(text = hint) },
             visualTransformation = visualTransformation,
+            supportingText = {
+                if (isError) {
+                    Text(text = getErrorMessage(value))
+                }
+            },
         )
-        if (isError) {
-            ErrorText(value, getErrorMessage)
-        }
     }
-}
-
-@Composable
-fun ErrorText(
-    value: String,
-    getErrorMessage: @Composable (String) -> String,
-) {
-    Text(
-        modifier = Modifier.padding(start = 16.dp),
-        text = getErrorMessage(value),
-        fontSize = 12.sp,
-        fontWeight = FontWeight.Normal,
-        color = Red,
-    )
 }
 
 @Preview(showBackground = true)
