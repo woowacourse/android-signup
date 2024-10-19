2dfa83aee5e26ae8c0165286433ce0b359a6a076
hxeyexn <kck85075@kumoh.ac.kr>
2024-10-19 17:21:07 +0900

    test: 사용자 이름 유효성 검사 테스트 추가 및 수정

--- app/src/test/java/nextstep/signup/model/UserNameTest.kt
+++ app/src/test/java/nextstep/signup/model/UserNameTest.kt
@@ -5,38 +5,80 @@ import org.junit.Test
 
 class UserNameTest {
     @Test
-    fun `사용자_이름이_2에서_5자면_에러_메시지는_null이_반환된다`() {
+    fun `사용자 이름이 2자 이상이고 5자 이하이면 에러 메시지는 null이 반환된다`() {
         val errorMessage: String? = UserName("해나").getErrorMessage()
         assertEquals(null, errorMessage)
     }
 
     @Test
-    fun `사용자_이름이_2에서_5자가 아니면_에러메시지가_반환된다`() {
+    fun `사용자 이름이 2자 미만이면 에러메시지가 반환된다`() {
+        val errorMessage: String? = UserName("h").getErrorMessage()
+        assertEquals(USERNAME_LENGTH_ERROR, errorMessage)
+    }
+
+    @Test
+    fun `사용자 이름이 5자 초과이면 에러메시지가 반환된다`() {
         val errorMessage: String? = UserName("hannah").getErrorMessage()
         assertEquals(USERNAME_LENGTH_ERROR, errorMessage)
     }
 
     @Test
-    fun `사용자_이름에_숫자나_기호가_포함되어_있지_않다면_에러_메시지는_null이_반환된다`() {
+    fun `사용자 이름에 숫자나 기호가 포함되어 있지 않다면 에러 메시지는 null이 반환된다`() {
         val errorMessage: String? = UserName("해나").getErrorMessage()
         assertEquals(null, errorMessage)
     }
 
     @Test
-    fun `사용자_이름에_숫자나_기호가_포함되어_있다면_에러메시지가_반환된다`() {
+    fun `사용자 이름에 숫자가 포함되어 있다면 에러메시지가 반환된다`() {
+        val errorMessage: String? = UserName("해나731").getErrorMessage()
+        assertEquals(USERNAME_FORM_ERROR, errorMessage)
+    }
+
+    @Test
+    fun `사용자 이름에 기호가 포함되어 있다면 에러메시지가 반환된다`() {
+        val errorMessage: String? = UserName("해나&#").getErrorMessage()
+        assertEquals(USERNAME_FORM_ERROR, errorMessage)
+    }
+
+    @Test
+    fun `사용자 이름에 숫자와 기호가 포함되어 있다면 에러메시지가 반환된다`() {
         val errorMessage: String? = UserName("해나1&").getErrorMessage()
         assertEquals(USERNAME_FORM_ERROR, errorMessage)
     }
 
     @Test
-    fun `사용자_이름이_2~5자이고_숫자나_기호가_포함되어_있지_않다면_isInvalid의_값은_false이다`() {
+    fun `사용자 이름이 2~5자이고 숫자나 기호가 포함되어 있지 않다면 isInvalid의 값은 false이다`() {
         val isInvalid: Boolean = UserName("해나").isInvalid()
         assertEquals(false, isInvalid)
     }
 
     @Test
+    fun `사용자 이름이 2자 미만이면 isInvalid의 값은 true이다`() {
+        val isInvalid: Boolean = UserName("h").isInvalid()
+        assertEquals(true, isInvalid)
+    }
+
+    @Test
+    fun `사용자 이름이 5자 초과이면 isInvalid의 값은 true이다`() {
+        val isInvalid: Boolean = UserName("hannah").isInvalid()
+        assertEquals(true, isInvalid)
+    }
+
+    @Test
+    fun `사용자 이름에 숫자가 포함되어 있다면 isInvalid의 값은 true이다`() {
+        val isInvalid: Boolean = UserName("해나731").isInvalid()
+        assertEquals(true, isInvalid)
+    }
+
+    @Test
+    fun `사용자 이름에 기호가 포함되어 있다면 isInvalid의 값은 true이다`() {
+        val isInvalid: Boolean = UserName("해나&#").isInvalid()
+        assertEquals(true, isInvalid)
+    }
+
+    @Test
     fun `사용자_이름이_2~5자_이상이고_숫자나_기호가_포함되어_있다면_isInvalid의_값은_true이다`() {
-        val isInvalid: Boolean = UserName("해나123&").isInvalid()
+        val isInvalid: Boolean = UserName("해나123&#").isInvalid()
         assertEquals(true, isInvalid)
     }
 
