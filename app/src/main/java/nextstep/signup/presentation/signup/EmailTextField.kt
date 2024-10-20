fa76dc865da6c42341e5e9fadfc8e11f01c0e1a5
sh1mj1 <7wlgns@gmail.com>
2024-10-21 00:32:03 +0900

    refactor(EmailTextField): name 이 변할 때 Email.from(email) 한번만

--- app/src/main/java/nextstep/signup/presentation/signup/EmailTextField.kt
+++ app/src/main/java/nextstep/signup/presentation/signup/EmailTextField.kt
@@ -2,6 +2,9 @@ package nextstep.signup.presentation.signup
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -19,21 +22,29 @@ fun EmailTextField(
     onValueChange: (String) -> Unit,
     email: String
 ) {
+    val emailResult: EmailResult by remember(email) {
+        mutableStateOf(Email.from(email))
+    }
+
     SignUpTextField(
         modifier = modifier,
         labelText = labelText,
         value = email,
         onValueChange = onValueChange,
         keyboardType = KeyboardType.Email,
-        isError = Email.from(email) is EmailResult.Failure,
-        supportingText = {
-            when (Email.from(input = email)) {
-                is EmailResult.EmptyField -> return@SignUpTextField
-                is EmailResult.Success -> return@SignUpTextField
-                is EmailResult.InvalidNameFormat -> Text(text = stringResource(id = R.string.error_message_email_invalid_format))
-            }
-        }
+        isError = emailResult is EmailResult.Failure,
+        supportingText = { ErrorText(emailResult) }
+
     )
+}
+
+@Composable
+private fun ErrorText(emailResult: EmailResult) {
+    return when (emailResult) {
+        is EmailResult.EmptyField -> return
+        is EmailResult.Success -> return
+        is EmailResult.InvalidNameFormat -> Text(text = stringResource(id = R.string.error_message_email_invalid_format))
+    }
 }
 
 @Preview
