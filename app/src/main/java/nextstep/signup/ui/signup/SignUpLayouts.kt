ddd5d7527fc88de23d6742c3ca530d578795089f
Junyoung-WON <wjyo60@gmail.com>
2024-10-20 18:25:55 +0900

    refactor: (4단계) 스낵바 적용 및 컴포지션 최적화 진행
    스낵바 활용
    - SnackBarHostState와 코루틴 스코프를 활용하여 SnackBarHost 구성
    - Box 레이아웃을 활용하여 스낵바를 레이아웃 위에 출력
    
    Recomposition Issue 해결
    - 하나의 TextField 입력 값 변경에도 다른 TextField들이 재구성되는 현상 제거
    - 상태 호이스팅 원칙, UDF 원칙 적용

--- app/src/main/java/nextstep/signup/ui/signup/SignUpLayouts.kt
+++ app/src/main/java/nextstep/signup/ui/signup/SignUpLayouts.kt
@@ -1,12 +1,15 @@
 package nextstep.signup.ui.signup
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,12 +17,15 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import nextstep.signup.R
 import nextstep.signup.domain.Email
 import nextstep.signup.domain.Password
@@ -43,12 +49,56 @@ fun SignUpTitle() {
 }
 
 @Composable
-fun SignUpInteractionLayer(onButtonClicked: () -> Unit) {
-    val textFieldModifier = Modifier.fillMaxWidth()
+fun SignUpInputs(
+    modifier: Modifier = Modifier,
+    username: Username,
+    email: Email,
+    password: Password,
+    passwordConfirm: String,
+    onUsernameChanged: (String) -> Unit,
+    onEmailChanged: (String) -> Unit,
+    onPasswordChanged: (String) -> Unit,
+    onPasswordConfirmChanged: (String) -> Unit,
+) {
+    SingleLineTextInput(
+        modifier = modifier,
+        value = username.value,
+        onValueChange = onUsernameChanged,
+        label = stringResource(id = R.string.signup_username),
+        inputType = InputType.Username,
+        supportingText = username.validateUsername(),
+    )
+    SingleLineTextInput(
+        modifier = modifier,
+        value = email.value,
+        onValueChange = onEmailChanged,
+        label = stringResource(id = R.string.signup_email),
+        inputType = InputType.Email,
+        supportingText = email.validateEmail(),
+    )
+    SingleLineTextInput(
+        modifier = modifier,
+        value = password.value,
+        onValueChange = onPasswordChanged,
+        label = stringResource(id = R.string.signup_password),
+        inputType = InputType.Password,
+        supportingText = password.validatePassword(),
+    )
+    SingleLineTextInput(
+        modifier = modifier,
+        value = passwordConfirm,
+        onValueChange = onPasswordConfirmChanged,
+        label = stringResource(id = R.string.signup_password_confirm),
+        inputType = InputType.Password,
+        supportingText = passwordConfirm.validatePasswordConfirm(password.value),
+    )
+}
 
-    var username by remember { mutableStateOf(Username("")) }
-    var email by remember { mutableStateOf(Email("")) }
-    var password by remember { mutableStateOf(Password("")) }
+@Composable
+fun SignUpInteractionLayer(onButtonClicked: () -> Unit) {
+    var username by remember { mutableStateOf(Username()) }
+    var email by remember { mutableStateOf(Email()) }
+    var password by remember { mutableStateOf(Password()) }
     var passwordConfirm by remember { mutableStateOf("") }
     val isFormatValid by remember {
         derivedStateOf {
@@ -61,40 +111,20 @@ fun SignUpInteractionLayer(onButtonClicked: () -> Unit) {
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        SingleLineTextInput(
-            modifier = textFieldModifier,
-            value = username.value,
-            onValueChange = { username = Username(it) },
-            label = stringResource(id = R.string.signup_username),
-            inputType = InputType.Username,
-            validateInput = { username.validateUsername() },
-        )
-        SingleLineTextInput(
-            modifier = textFieldModifier,
-            value = email.value,
-            onValueChange = { email = Email(it) },
-            label = stringResource(id = R.string.signup_email),
-            inputType = InputType.Email,
-            validateInput = { email.validateEmail() },
-        )
-        SingleLineTextInput(
-            modifier = textFieldModifier,
-            value = password.value,
-            onValueChange = { password = Password(it) },
-            label = stringResource(id = R.string.signup_password),
-            inputType = InputType.Password,
-            validateInput = { password.validatePassword() },
-        )
-        SingleLineTextInput(
-            modifier = textFieldModifier,
-            value = passwordConfirm,
-            onValueChange = { passwordConfirm = it },
-            label = stringResource(id = R.string.signup_password_confirm),
-            inputType = InputType.Password,
-            validateInput = { passwordConfirm.validatePasswordConfirm(password.value) },
+        val modifier = Modifier.fillMaxWidth()
+        SignUpInputs(
+            modifier = modifier,
+            username = username,
+            email = email,
+            password = password,
+            passwordConfirm = passwordConfirm,
+            onUsernameChanged = { username = username.copy(value = it) },
+            onEmailChanged = { email = email.copy(value = it) },
+            onPasswordChanged = { password = password.copy(value = it) },
+            onPasswordConfirmChanged = { passwordConfirm = it }
         )
         StateButton(
-            modifier = textFieldModifier.requiredHeight(50.dp),
+            modifier = modifier.requiredHeight(50.dp),
             text = stringResource(id = R.string.signup_signup),
             enabled = isFormatValid,
         ) {
@@ -104,7 +134,16 @@ fun SignUpInteractionLayer(onButtonClicked: () -> Unit) {
 }
 
 @Composable
-fun SignUpLayout(onButtonClicked: () -> Unit) {
+fun SignUpLayout() {
+    val snackbarHostState by remember { mutableStateOf(SnackbarHostState()) }
+    val coroutineScope = rememberCoroutineScope()
+    val localContextResources = LocalContext.current.resources
+    val showSignUpSuccessSnackbar: () -> Unit = {
+        coroutineScope.launch {
+            snackbarHostState.showSnackbar(localContextResources.getString(R.string.signup_signup_success))
+        }
+    }
+
     Column(
         modifier =
             Modifier
@@ -114,8 +153,14 @@ fun SignUpLayout(onButtonClicked: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         SignUpTitle()
-        SignUpInteractionLayer {
-            onButtonClicked()
+        Box {
+            SignUpInteractionLayer {
+                showSignUpSuccessSnackbar()
+            }
+            SnackbarHost(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                hostState = snackbarHostState,
+            )
         }
     }
 }
@@ -130,7 +175,7 @@ fun SignUpLayoutPreview() {
                     .fillMaxSize()
                     .padding(32.dp),
         ) {
-            SignUpInteractionLayer {}
+            SignUpLayout()
         }
     }
 }
