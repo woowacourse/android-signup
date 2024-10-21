8aefd12426b82a9ef8e56460699d262de5248ae1
JunJangE <wnswkd486@gmail.com>
2024-10-21 16:09:41 +0900

    refactor: CustomText 컴포저블 제거

--- app/src/main/java/nextstep/signup/presentation/ui/MainScreen.kt
+++ app/src/main/java/nextstep/signup/presentation/ui/MainScreen.kt
@@ -7,22 +7,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nextstep.signup.R
 import nextstep.signup.domain.Email
 import nextstep.signup.domain.Password
 import nextstep.signup.domain.UserName
 import nextstep.signup.presentation.componet.CustomButton
-import nextstep.signup.presentation.componet.CustomText
 import nextstep.signup.presentation.componet.CustomTextField
 import nextstep.signup.presentation.ui.theme.Blue50
 
@@ -32,11 +36,14 @@ fun MainScreen() {
 
     Column {
         Spacer(modifier = Modifier.height(60.dp))
-        CustomText(
+        Text(
             modifier = Modifier.fillMaxWidth(),
-            title = stringResource(R.string.sign_up_title),
+            text = stringResource(R.string.sign_up_title),
+            fontSize = 26.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.W700,
+            textAlign = TextAlign.Center,
         )
-
         Spacer(modifier = Modifier.height(18.dp))
         CustomTextField(
             modifier = Modifier.padding(horizontal = 32.dp, vertical = 18.dp),
