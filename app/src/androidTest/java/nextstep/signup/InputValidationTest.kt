229f6198a094c43823751e698e3d130ab2a2bd5b
kkosang <eiwoqp77@gmail.com>
2024-10-22 00:35:14 +0900

    refactor: InputValidationTest 수정

--- app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import nextstep.signup.component.SignUpEmailTextField
 import nextstep.signup.component.SignUpPasswordConfirmTextField
 import nextstep.signup.component.SignUpPasswordTextField
@@ -15,6 +16,7 @@ import nextstep.signup.domain.validation.Password
 import nextstep.signup.domain.validation.PasswordConfirm
 import nextstep.signup.domain.validation.SignUpState
 import nextstep.signup.domain.validation.Username
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -26,15 +28,33 @@ class InputValidationTest {
     private val password = mutableStateOf("")
     private val passwordConfirm = mutableStateOf("")
 
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            SignUpUsernameTextField(username = username.value) {
+                username.value = it
+            }
+            SignUpEmailTextField(email = email.value) {
+                email.value = it
+            }
+            SignUpPasswordTextField(password = password.value) {
+                password.value = it
+            }
+            SignUpPasswordConfirmTextField(
+                password = password.value,
+                passwordConfirm = passwordConfirm.value
+            ) {
+                passwordConfirm.value = it
+            }
+        }
+    }
+
     @Test
     fun 사용자가_이름을_입력하기_전에는_에러가_노출되지_않는다() {
-        // given
-        username.value = ""
-
         // when
-        composeTestRule.setContent {
-            SignUpUsernameTextField(username = username.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Username")
+            .performTextInput("")
 
         // then
         composeTestRule
@@ -48,13 +68,10 @@ class InputValidationTest {
 
     @Test
     fun 사용자_이름의_글자_수는_2에서_5자여야_한다() {
-        // given
-        username.value = "꼬상"
-
         // when
-        composeTestRule.setContent {
-            SignUpUsernameTextField(username = username.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Username")
+            .performTextInput("꼬상")
 
         // then
         composeTestRule
@@ -64,13 +81,10 @@ class InputValidationTest {
 
     @Test
     fun 사용자_이름이_2에서_5자가_아니면_에러메시지가_노출된다() {
-        // given
-        username.value = "포켓로그헬퍼"
-
         // when
-        composeTestRule.setContent {
-            SignUpUsernameTextField(username = username.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Username")
+            .performTextInput("포켓로그헬퍼")
 
         // then
         composeTestRule
@@ -80,13 +94,10 @@ class InputValidationTest {
 
     @Test
     fun 사용자_이름에_숫자가_포함되면_에러메시지가_노출된다() {
-        // given
-        username.value = "꼬상2"
-
         // when
-        composeTestRule.setContent {
-            SignUpUsernameTextField(username = username.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Username")
+            .performTextInput("꼬상2")
 
         // then
         composeTestRule
@@ -96,13 +107,10 @@ class InputValidationTest {
 
     @Test
     fun 사용자_이름에_기호가_포함되면_에러메시지가_노출된다() {
-        // given
-        username.value = "꼬상!"
-
         // when
-        composeTestRule.setContent {
-            SignUpUsernameTextField(username = username.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Username")
+            .performTextInput("꼬상!")
 
         // then
         composeTestRule
@@ -112,13 +120,10 @@ class InputValidationTest {
 
     @Test
     fun 이메일은_이메일의_형식을_지켜야한다() {
-        // given
-        email.value = "kshyun0724@naver.com"
-
         // when
-        composeTestRule.setContent {
-            SignUpEmailTextField(email = email.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("kshyun0724@naver.com")
 
         // then
         composeTestRule
@@ -128,13 +133,10 @@ class InputValidationTest {
 
     @Test
     fun 이메일_형식이_올바르지_않으면_에러메시지가_노출된다_1() {
-        // given
-        email.value = "kshyun0724naver.com"
-
         // when
-        composeTestRule.setContent {
-            SignUpEmailTextField(email = email.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("kshyun0724naver.com")
 
         // then
         composeTestRule
@@ -144,13 +146,10 @@ class InputValidationTest {
 
     @Test
     fun 이메일_형식이_올바르지_않으면_에러메시지가_노출된다_2() {
-        // given
-        email.value = "kshyun0724@navercom"
-
         // when
-        composeTestRule.setContent {
-            SignUpEmailTextField(email = email.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Email")
+            .performTextInput("kshyun0724@navercom")
 
         // then
         composeTestRule
@@ -161,12 +160,9 @@ class InputValidationTest {
     @Test
     fun 비밀번호는_8에서_16자여야_한다() {
         // given
-        password.value = "12345678"
-
-        // when
-        composeTestRule.setContent {
-            SignUpPasswordTextField(password = password.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Password")
+            .performTextInput("12345678")
 
         // then
         composeTestRule
@@ -176,13 +172,10 @@ class InputValidationTest {
 
     @Test
     fun 비밀번호는_영문과_숫자를_포함해야한다() {
-        // given
-        password.value = "1234567a"
-
         // when
-        composeTestRule.setContent {
-            SignUpPasswordTextField(password = password.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Password")
+            .performTextInput("1234567a")
 
         // then
         composeTestRule
@@ -192,13 +185,10 @@ class InputValidationTest {
 
     @Test
     fun 비밀번호_형식이_올바르지_않으면_에러메시지가_노출된다() {
-        // given
-        password.value = "12345678"
-
         // when
-        composeTestRule.setContent {
-            SignUpPasswordTextField(password = password.value) {}
-        }
+        composeTestRule
+            .onNodeWithText("Password")
+            .performTextInput("12345678")
 
         // then
         composeTestRule
@@ -210,15 +200,11 @@ class InputValidationTest {
     fun 비밀번호와_비밀번호확인은_일치해야한다() {
         // given
         password.value = "1234567a"
-        passwordConfirm.value = "1234567a"
 
         // when
-        composeTestRule.setContent {
-            SignUpPasswordConfirmTextField(
-                password = password.value,
-                passwordConfirm = passwordConfirm.value
-            ) {}
-        }
+        composeTestRule
+            .onNodeWithText("Password Confirm")
+            .performTextInput("1234567a")
 
         // then
         composeTestRule
@@ -230,70 +216,15 @@ class InputValidationTest {
     fun 비밀번호와_비밀번호확인이_다르면_에러메시지가_노출된다() {
         // given
         password.value = "1234567a"
-        passwordConfirm.value = "1234567b"
 
         // when
-        composeTestRule.setContent {
-            SignUpPasswordConfirmTextField(
-                password = password.value,
-                passwordConfirm = passwordConfirm.value
-            ) {}
-        }
+        composeTestRule.onNodeWithText("Password Confirm")
+            .performTextInput("1234567b")
 
         // then
         composeTestRule
             .onNodeWithText(ERROR_PASSWORD_CONFIRM)
             .assertExists()
-    }
-
-    @Test
-    fun 유효성_검사에_하나라도_실패하면_버튼이_비활성화_상태로_노출된다() {
-        // given
-        val invalidName = "k"
-        val validEmail = "kshyun@naver.com"
-        val validPassword = "1234567a"
-        val validPasswordConfirm = "1234567a"
-        val signUpState = SignUpState(
-            username = Username(invalidName),
-            email = Email(validEmail),
-            password = Password(validPassword),
-            passwordConfirm = PasswordConfirm(validPassword, validPasswordConfirm)
-        )
-
-        // when
-        composeTestRule.setContent {
-            SignUpSubmitButton(text = "sign up", onClick = { }, enabled = signUpState.isValid())
-        }
-
-        // then
-        composeTestRule
-            .onNodeWithText("sign up")
-            .assertIsNotEnabled()
-    }
-
-    @Test
-    fun 유효성_검사에_모두_통과하면_버튼이_활성화_상태로_노출된다() {
-        // given
-        val validName = "sang"
-        val validEmail = "kshyun@naver.com"
-        val validPassword = "1234567a"
-        val validPasswordConfirm = "1234567a"
-        val signUpState = SignUpState(
-            username = Username(validName),
-            email = Email(validEmail),
-            password = Password(validPassword),
-            passwordConfirm = PasswordConfirm(validPassword, validPasswordConfirm)
-        )
-
-        // when
-        composeTestRule.setContent {
-            SignUpSubmitButton(text = "sign up", onClick = { }, enabled = signUpState.isValid())
-        }
-
-        // then
-        composeTestRule
-            .onNodeWithText("sign up")
-            .assertIsEnabled()
     }
 
     companion object {
