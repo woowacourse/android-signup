e528313d6edfd9ebc92ef90c930a8f61ab732da7
dpcks0509 <dpcks0509@gmail.com>
2024-10-21 15:38:44 +0900

    feat: HeaderTextPreviewParameter를 이용한 Preview 추가

--- app/src/main/java/nextstep/signup/ui/component/HeaderText.kt
+++ app/src/main/java/nextstep/signup/ui/component/HeaderText.kt
@@ -4,6 +4,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -17,4 +20,20 @@ fun HeaderText(
         fontSize = 26.sp,
         fontWeight = FontWeight.Bold,
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderTextPreview(
+    @PreviewParameter(HeaderTextPreviewParameterProvider::class) text: String,
+) {
+    HeaderText(text = text)
+}
+
+class HeaderTextPreviewParameterProvider : PreviewParameterProvider<String> {
+    override val values: Sequence<String> =
+        sequenceOf(
+            "채드",
+            "심지",
+        )
 }
