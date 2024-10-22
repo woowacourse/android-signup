99bd67b70a4d37266cd57c2a7030a71899722209
hxeyexn <kck85075@kumoh.ac.kr>
2024-10-22 14:30:31 +0900

    refactor: PASSWORD_REGEX 정규 표현식 수정

--- app/src/main/java/nextstep/signup/model/Password.kt
+++ app/src/main/java/nextstep/signup/model/Password.kt
@@ -20,7 +20,7 @@ data class Password(val password: String) {
     private fun hasEnglishAndNumber(): Boolean = password.matches(Regex(PASSWORD_REGEX))
 
     companion object {
-        private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$"
+        private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).*$"
         private const val PASSWORD_LENGTH_ERROR = "비밀번호는 8~16자여야 합니다."
         private const val PASSWORD_FORM_ERROR = "비밀번호는 영문과 숫자를 포함해야 합니다."
         private const val PASSWORD_MIN_LENGTH = 8
