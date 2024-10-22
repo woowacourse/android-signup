44b8f80b605759adbe5d93b0106327b2563996d6
ii2001 <iicks2001@gmail.com>
2024-10-22 10:31:27 +0900

    :white_check_mark: test before로 정리

--- app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
+++ app/src/androidTest/java/nextstep/signup/InputValidationTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import nextstep.signup.ui.Validator.getUserNameError
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,12 +13,11 @@ class InputValidationTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun username_with_exactly_2_characters_shows_no_error() {
-        // given
-        val userName = mutableStateOf("")
-        val userNameError = mutableStateOf("")
+    private val userName = mutableStateOf("")
+    private val userNameError = mutableStateOf("")
 
+    @Before
+    fun setUp() {
         composeTestRule.setContent {
             TestFixture.TestCustomTextField(
                 valueState = userName,
@@ -32,7 +32,10 @@ class InputValidationTest {
                 }
             )
         }
+    }
 
+    @Test
+    fun username_with_exactly_2_characters_shows_no_error() {
         // when
         composeTestRule.runOnUiThread {
             userName.value = "길동"
@@ -46,25 +49,6 @@ class InputValidationTest {
 
     @Test
     fun username_with_exactly_5_characters_shows_no_error() {
-        // given
-        val userName = mutableStateOf("")
-        val userNameError = mutableStateOf("")
-
-        composeTestRule.setContent {
-            TestFixture.TestCustomTextField(
-                valueState = userName,
-                errorState = userNameError,
-                label = "Username",
-                onValueChange = {
-                    userNameError.value = getUserNameError(
-                        it,
-                        TestFixture.USERNAME_LENGTH_ERROR,
-                        TestFixture.USERNAME_FORMAT_ERROR
-                    )
-                }
-            )
-        }
-
         // when
         composeTestRule.runOnUiThread {
             userName.value = "김철수김"
