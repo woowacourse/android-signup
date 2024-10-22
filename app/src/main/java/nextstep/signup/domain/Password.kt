fbc8d103176bd64758e636d48eca8a1ceb9c10d9
Junyoung-WON <wjyo60@gmail.com>
2024-10-22 14:17:10 +0900

    refactor: (4단계) Password 내부 원시값들의 상수화 진행
    - 프로퍼티 디폴트 값, 글자 수 상수화 진행
    - 상수값 오타 수정

--- app/src/main/java/nextstep/signup/domain/Password.kt
+++ app/src/main/java/nextstep/signup/domain/Password.kt
@@ -1,6 +1,6 @@
 package nextstep.signup.domain
 
-data class Password(val value: String = "") {
+data class Password(val value: String = EMPTY_PASSWORD) {
     val isValid: Boolean = isValidLength && isValidFormat
 
     val isValidLength: Boolean
@@ -9,12 +9,15 @@ data class Password(val value: String = "") {
     val isValidFormat: Boolean
         get() = value.validatePasswordFormat()
 
-    private fun String.validatePasswordLength(): Boolean = length in PASSWORD_LEGNTH
+    private fun String.validatePasswordLength(): Boolean = length in PASSWORD_LENGTH
 
     private fun String.validatePasswordFormat(): Boolean = matches(Regex(PASSWORD_REGEX))
 
     companion object {
+        private const val EMPTY_PASSWORD = ""
+        private const val MIN_PASSWORD_LENGTH = 8
+        private const val MAX_PASSWORD_LENGTH = 16
+        private val PASSWORD_LENGTH = MIN_PASSWORD_LENGTH..MAX_PASSWORD_LENGTH
         private const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9]).*$"
-        private val PASSWORD_LEGNTH = 8..16
     }
 }
