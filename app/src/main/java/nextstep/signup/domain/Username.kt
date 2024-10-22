6a62fc76e37633846d9e0dc2eeedab17b0e3c821
Junyoung-WON <wjyo60@gmail.com>
2024-10-22 14:17:18 +0900

    refactor: (4단계) Username 내부 원시값들의 상수화 진행
    - 프로퍼티 디폴트 값, 글자 수 상수화 진행
    - 상수값 오타 수정

--- app/src/main/java/nextstep/signup/domain/Username.kt
+++ app/src/main/java/nextstep/signup/domain/Username.kt
@@ -1,6 +1,6 @@
 package nextstep.signup.domain
 
-data class Username(val value: String = "") {
+data class Username(val value: String = EMPTY_USERNAME) {
     val isValid: Boolean = isValidLength && isValidFormat
 
     val isValidLength: Boolean
@@ -9,12 +9,15 @@ data class Username(val value: String = "") {
     val isValidFormat: Boolean
         get() = value.validateUsernameFormat()
 
-    private fun String.validateUsernameLength(): Boolean = length in USERNAME_LEGNTH
+    private fun String.validateUsernameLength(): Boolean = length in USERNAME_LENGTH
 
     private fun String.validateUsernameFormat(): Boolean = matches(Regex(USERNAME_REGEX))
 
     companion object {
+        private const val EMPTY_USERNAME = ""
+        private const val MIN_USERNAME_LENGTH = 2
+        private const val MAX_USERNAME_LENGTH = 5
+        private val USERNAME_LENGTH = MIN_USERNAME_LENGTH..MAX_USERNAME_LENGTH
         private const val USERNAME_REGEX = "^[a-zA-Z가-힣]+$"
-        private val USERNAME_LEGNTH = 2..5
     }
 }
