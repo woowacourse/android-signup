06747b29abd695f726e4ab89de96ac8d6a002fdd
JoYehyun <jo990714@naver.com>
2024-10-22 14:43:20 +0900

    test: UI 테스트 분리

---
+++ app/src/androidTest/java/nextstep/signup/PasswordConfirmInputTextTest.kt
@@ -0,0 +1,54 @@
+package nextstep.signup
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import nextstep.signup.model.PasswordConfirm
+import nextstep.signup.ui.component.InputText
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PasswordConfirmInputTextTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private val password = "password12"
+    private var passwordConfirm by mutableStateOf(PasswordConfirm("", password = password))
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            InputText(
+                title = R.string.sign_up_user_name_title,
+                content = passwordConfirm.content,
+                inputValidator = passwordConfirm,
+            )
+        }
+    }
+
+    @Test
+    fun `비밀번호_확인란_값이_비밀번호와_같지않은_경우_에러메세지가_표시된다`() {
+        // when
+        passwordConfirm = passwordConfirm.copy(content = "password34")
+        composeTestRule.waitForIdle()
+
+        // then
+        composeTestRule
+            .onNodeWithText(PasswordConfirm.ERROR_PASSWORD_CONFIRM_MESSAGE)
+            .assertExists()
+    }
+
+    @Test
+    fun `비밀번호_확인란의_값은_비밀번호와_같아야한다`() {
+        // when
+        passwordConfirm = passwordConfirm.copy(content = "password12")
+        composeTestRule.waitForIdle()
+
+        // then
+        composeTestRule
+            .onNodeWithText(PasswordConfirm.ERROR_PASSWORD_CONFIRM_MESSAGE)
+            .assertDoesNotExist()
+    }
+}
