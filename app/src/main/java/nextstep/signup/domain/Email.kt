6d82936b617b1dea52e8f64b4c452c88712f66d0
Junyoung-WON <wjyo60@gmail.com>
2024-10-22 14:16:37 +0900

    refactor: (4단계) Email 내부 원시값들의 상수화 진행
    - 프로퍼티 디폴트 값 상수화 진행

--- app/src/main/java/nextstep/signup/domain/Email.kt
+++ app/src/main/java/nextstep/signup/domain/Email.kt
@@ -1,6 +1,6 @@
 package nextstep.signup.domain
 
-data class Email(val value: String = "") {
+data class Email(val value: String = EMPTY_EMAIL) {
     val isValid = isValidFormat
 
     val isValidFormat: Boolean
@@ -9,6 +9,7 @@ data class Email(val value: String = "") {
     private fun String.validateEmailFormat(): Boolean = matches(Regex(EMAIL_REGEX))
 
     companion object {
+        private const val EMPTY_EMAIL = ""
         private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
     }
 }
