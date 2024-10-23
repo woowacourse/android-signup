66a19bb11b54550e31a31024c2e80d76f2bf4d6d
hxeyexn <kck85075@kumoh.ac.kr>
2024-10-23 13:24:47 +0900

    test: 테스트명을 기능에 초점을 맞춰 변경

--- app/src/test/java/nextstep/signup/model/PasswordTest.kt
+++ app/src/test/java/nextstep/signup/model/PasswordTest.kt
@@ -5,85 +5,85 @@ import org.junit.Test
 
 class PasswordTest {
     @Test
-    fun `비밀번호가 8자 이상 16자 이하라면 PasswordValidResult는 Success가 반환된다`() {
+    fun `비밀번호가 8자 이상 16자 이하라면 유효성 검증에 성공한다`() {
         val passwordValidResult: PasswordValidResult = Password("hannah0731").validate()
         assertEquals(PasswordValidResult.Success, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 8자 미만이라면 PasswordValidResult는 InvalidLength가 반환된다`() {
+    fun `비밀번호가 8자 미만이라면 길이 유효성 검증에 실패한다`() {
         val passwordValidResult: PasswordValidResult = Password("hye731").validate()
         assertEquals(PasswordValidResult.InvalidLength, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 16자 초과라면 PasswordValidResult는 InvalidLength가 반환된다`() {
+    fun `비밀번호가 16자 초과라면 길이 유효성 검증에 실패한다`() {
         val passwordValidResult: PasswordValidResult = Password("hannah731gonghyeyeon").validate()
         assertEquals(PasswordValidResult.InvalidLength, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 영문과 숫자를 포함하고 있다면 PasswordValidResult는 Success가 반환된다`() {
+    fun `비밀번호가 영문과 숫자를 포함하고 있다면 유효성 검증에 성공한다`() {
         val passwordValidResult: PasswordValidResult = Password("hannah0731").validate()
         assertEquals(PasswordValidResult.Success, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 숫자를 포함하지 않으면 PasswordValidResult는 InvalidCharacter가 반환된다`() {
+    fun `비밀번호가 숫자를 포함하지 않으면 문자 유효성 검증에 실패한다`() {
         val passwordValidResult: PasswordValidResult = Password("gonghyeyeon").validate()
         assertEquals(PasswordValidResult.InvalidCharacter, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 영문을 포함하지 않으면 PasswordValidResult는 InvalidCharacter가 반환된다`() {
+    fun `비밀번호가 영문을 포함하지 않으면 문자 유효성 검증에 실패한다`() {
         val passwordValidResult: PasswordValidResult = Password("1234567899").validate()
         assertEquals(PasswordValidResult.InvalidCharacter, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 영문과 숫자를 포함하지 않으면 PasswordValidResult는 InvalidCharacter가 반환된다`() {
+    fun `비밀번호가 영문과 숫자를 포함하지 않으면 문자 유효성 검증에 실패한다`() {
         val passwordValidResult: PasswordValidResult = Password("해나해나해나@#$").validate()
         assertEquals(PasswordValidResult.InvalidCharacter, passwordValidResult)
     }
 
     @Test
-    fun `비밀번호가 8~16자이고 영문과 숫자를 포함하고 있다면 isValid의 값은 true이다`() {
+    fun `비밀번호가 8~16자이고 영문과 숫자를 포함하고 있다면 유효하다`() {
         val isValid: Boolean = Password("hannah0731").isValid()
         assertEquals(true, isValid)
     }
 
     @Test
-    fun `비밀번호가 8자 미만이라면 isValid의 값은 false이다`() {
+    fun `비밀번호가 8자 미만이라면 유효하지 않다`() {
         val isValid: Boolean = Password("hye731").isValid()
         assertEquals(false, isValid)
     }
 
     @Test
-    fun `비밀번호가 16자 초과라면 isValid의 값은 false이다`() {
+    fun `비밀번호가 16자 초과라면 유효하지 않`() {
         val isValid: Boolean = Password("hannah731gonghyeyeon").isValid()
         assertEquals(false, isValid)
     }
 
     @Test
-    fun `비밀번호가 숫자를 포함하지 않으면 isValid의 값은 false이다`() {
+    fun `비밀번호가 숫자를 포함하지 않으면 유효하지 않다`() {
         val isValid: Boolean = Password("gonghyeyeon").isValid()
         assertEquals(false, isValid)
     }
 
     @Test
-    fun `비밀번호가 영문을 포함하지 않으면 isValid의 값은 false이다`() {
+    fun `비밀번호가 영문을 포함하지 않으면 유효하지 않다`() {
         val isValid: Boolean = Password("1234567899").isValid()
         assertEquals(false, isValid)
     }
 
     @Test
-    fun `비밀번호가 8~16자가 아니고 영문과 숫자를 포함하고 있지 않다면 isValid의 값은 false이다`() {
+    fun `비밀번호가 8~16자가 아니고 영문과 숫자를 포함하고 있지 않다면 유효하지 않다`() {
         val isValid: Boolean = Password("해나@#\$").isValid()
         assertEquals(false, isValid)
     }
 
     @Test
-    fun `비밀번호가 비어있다면 isError의 값은 false이다`() {
+    fun `비밀번호가 비어있는 상태는 오류가 아니다`() {
         val isError: Boolean = Password("").isError()
         assertEquals(false, isError)
     }
--- app/src/test/java/nextstep/signup/model/UserNameTest.kt
+++ app/src/test/java/nextstep/signup/model/UserNameTest.kt
@@ -7,85 +7,85 @@ import org.junit.Test
 
 class UserNameTest {
     @Test
-    fun `사용자 이름이 2자 이상이고 5자 이하이면 userNameValidResult는 Success가 반환된다`() {
+    fun `사용자 이름이 2자 이상이고 5자 이하이면 유효성 검증에 성공한다`() {
         val userNameValidResult: UserNameValidResult = UserName("해나").validate()
         assertEquals(UserNameValidResult.Success, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름이 2자 미만이면 userNameValidResult는 InvalidLength가 반환된다`() {
+    fun `사용자 이름이 2자 미만이면 길이 유효성 검증에 실패한다`() {
         val userNameValidResult: UserNameValidResult = UserName("h").validate()
         assertEquals(UserNameValidResult.InvalidLength, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름이 5자 초과이면 userNameValidResult는 InvalidLength가 반환된다`() {
+    fun `사용자 이름이 5자 초과이면 길이 유효성 검증에 실패한다`() {
         val userNameValidResult: UserNameValidResult = UserName("hannah").validate()
         assertEquals(UserNameValidResult.InvalidLength, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름에 숫자나 기호가 포함되어 있지 않다면 userNameValidResult는 Success가 반환된다`() {
+    fun `사용자 이름에 숫자나 기호가 포함되어 있지 않다면 유효성 검증에 성공한다`() {
         val userNameValidResult: UserNameValidResult = UserName("해나").validate()
         assertEquals(UserNameValidResult.Success, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름에 숫자가 포함되어 있다면 userNameValidResult는 InvalidCharacter가 반환된다`() {
+    fun `사용자 이름에 숫자가 포함되어 있다면 문자 유효성 검증에 실패한다`() {
         val userNameValidResult: UserNameValidResult = UserName("해나731").validate()
         assertEquals(UserNameValidResult.InvalidCharacter, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름에 기호가 포함되어 있다면 userNameValidResult는 InvalidCharacter가 반환된다`() {
+    fun `사용자 이름에 기호가 포함되어 있다면 문자 유효성 검증에 실패한다`() {
         val userNameValidResult: UserNameValidResult = UserName("해나&#").validate()
         assertEquals(UserNameValidResult.InvalidCharacter, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름에 숫자와 기호가 포함되어 있다면 userNameValidResult는 InvalidCharacter가 반환된다`() {
+    fun `사용자 이름에 숫자와 기호가 포함되어 있다면 문자 유효성 검증에 실패한다`() {
         val userNameValidResult: UserNameValidResult = UserName("해나1&").validate()
         assertEquals(UserNameValidResult.InvalidCharacter, userNameValidResult)
     }
 
     @Test
-    fun `사용자 이름이 2~5자이고 숫자나 기호가 포함되어 있지 않다면 isValid의 값은 true이다`() {
+    fun `사용자 이름이 2~5자이고 숫자나 기호가 포함되어 있지 않다면 유효하다`() {
         val isValid: Boolean = UserName("해나").isValid()
         assertTrue(isValid)
     }
 
     @Test
-    fun `사용자 이름이 2자 미만이면 isValid의 값은 false이다`() {
+    fun `사용자 이름이 2자 미만이면 유효하지 않다`() {
         val isValid: Boolean = UserName("h").isValid()
         assertFalse(isValid)
     }
 
     @Test
-    fun `사용자 이름이 5자 초과이면 isValid의 값은 false이다`() {
+    fun `사용자 이름이 5자 초과이면 유효하지 않다`() {
         val isValid: Boolean = UserName("hannah").isValid()
         assertFalse(isValid)
     }
 
     @Test
-    fun `사용자 이름에 숫자가 포함되어 있다면 isValid의 값은 false이다`() {
+    fun `사용자 이름에 숫자가 포함되어 있다면 유효하지 않다`() {
         val isValid: Boolean = UserName("해나731").isValid()
         assertFalse(isValid)
     }
 
     @Test
-    fun `사용자 이름에 기호가 포함되어 있다면 isValid의 값은 false이다`() {
+    fun `사용자 이름에 기호가 포함되어 있다면 유효하지 않다`() {
         val isValid: Boolean = UserName("해나&#").isValid()
         assertFalse(isValid)
     }
 
     @Test
-    fun `사용자 이름이 2~5자 이상이고 숫자나 기호가 포함되어 있다면 isValid의 값은 false이다`() {
+    fun `사용자 이름이 2~5자 이상이고 숫자나 기호가 포함되어 있다면 유효하지 않다`() {
         val isValid: Boolean = UserName("해나123&#").isValid()
         assertFalse(isValid)
     }
 
     @Test
-    fun `사용자 이름이 비어있다면 isError의 값은 false이다`() {
+    fun `사용자 이름이 비어있는 상태는 오류가 아니다`() {
         val isError: Boolean = UserName("").isError()
         assertFalse(isError)
     }
